d7343148744ba43cd03f37207c1e0dfd5562e4a1
xingfeng01 <xingfeng@baidu.com>
2021-11-15 10:15:04 +0000

    add wheel package setup

--- paddlescience/__init__.py
+++ paddlescience/__init__.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__version__ = '0.1'
+
 from . import geometry
 from . import pde
 from . import network
---
+++ setup.py
@@ -0,0 +1,72 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import setuptools
+import sys
+import paddlescience
+long_description = "PaddlePaddle AI-driven Scientific Computing SDK Library"
+
+with open("requirements.txt") as fin:
+    REQUIRED_PACKAGES = fin.read()
+
+
+def get_package_data_files(package, data, package_dir=None):
+    """
+    Helps to list all specified files in package including files in directories
+    since `package_data` ignores directories.
+    """
+    if package_dir is None:
+        package_dir = os.path.join(*package.split('.'))
+    all_files = []
+    for f in data:
+        path = os.path.join(package_dir, f)
+        if os.path.isfile(path):
+            all_files.append(f)
+            continue
+        for root, _dirs, files in os.walk(path, followlinks=True):
+            root = os.path.relpath(root, package_dir)
+            for file in files:
+                file = os.path.join(root, file)
+                if file not in all_files:
+                    all_files.append(file)
+    return all_files
+
+
+setuptools.setup(
+    name="paddlescience",
+    version=paddlescience.__version__,
+    author="PaddlePaddle Scientific Computing Team",
+    author_email="paddlescience@baidu.com",
+    description=long_description,
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    url="https://github.com/PaddlePaddle/PaddleScience",
+    packages=setuptools.find_packages(
+        where='.', exclude=('examples*', 'tests*')),
+    package_data={
+        'paddlescience': get_package_data_files('paddlescience', [
+            'README.md'])
+    },
+    setup_requires=['cython', 'numpy'],
+    install_requires=REQUIRED_PACKAGES,
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    license='Apache 2.0')
