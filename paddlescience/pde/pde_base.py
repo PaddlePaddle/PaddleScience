d54fb527d34bda83492688afeda8b5100d001b7d
xingfeng01 <xingfeng@baidu.com>
2021-11-15 15:07:21 +0000

    3d ns

--- paddlescience/pde/pde_base.py
+++ paddlescience/pde/pde_base.py
@@ -23,6 +23,7 @@ import paddle
 first_order_rslts = ['u', 'v', 'p']
 first_order_derivatives = [['du/dt', 'du/dx', 'du/dy', 'du/dz'],
                            ['dv/dt', 'dv/dx', 'dv/dy', 'dv/dz'],
+                           ['dw/dt', 'dw/dx', 'dw/dy', 'dw/dz'],
                            ['dp/dt', 'dp/dx', 'dp/dy', 'dp/dz']]
 
 #NOTE(lml): do not change this list
@@ -34,6 +35,10 @@ second_order_derivatives = [[['d2u/dt2', 'd2u/dtdx', 'd2u/dtdy', 'd2u/dtdz'],
                              ['d2v/dxdt', 'd2v/dx2', 'd2v/dxdy', 'd2v/dxdz'],
                              ['d2v/dydt', 'd2v/dydx', 'd2v/dy2', 'd2v/dydz'],
                              ['d2v/dzdt', 'd2v/dzdx', 'd2v/dzdy', 'd2v/dz2']],
+                            [['d2w/dt2', 'd2w/dtdx', 'd2w/dtdy', 'd2w/dtdz'],
+                             ['d2w/dxdt', 'd2w/dx2', 'd2w/dxdy', 'd2w/dxdz'],
+                             ['d2w/dydt', 'd2w/dydx', 'd2w/dy2', 'd2w/dydz'],
+                             ['d2w/dzdt', 'd2w/dzdx', 'd2w/dzdy', 'd2w/dz2']],
                             [['d2p/dt2', 'd2p/dtdx', 'd2p/dtdy', 'd2p/dtdz'],
                              ['d2p/dxdt', 'd2p/dx2', 'd2p/dxdy', 'd2p/dxdz'],
                              ['d2p/dydt', 'd2p/dydx', 'd2p/dy2', 'd2p/dydz'],
