8b15aa707eda2f37a207d734b435d6ea853aeeab
levi131 <limaolin01@baidu.com>
2021-11-18 09:17:40 +0000

    revert lfbgs

--- paddlescience/network/network_fc.py
+++ paddlescience/network/network_fc.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 import paddle
-from paddle.nn.initializer import Assign
 import paddle.nn.functional as F
 from .network_base import NetworkBase
 
@@ -59,47 +58,13 @@ class FCNet(NetworkBase):
             self.biases.append(b)
             self.add_parameter("w_" + str(i), w)
             self.add_parameter("b_" + str(i), b)
-            
 
     def nn_func(self, ins):
         u = ins
         for i in range(self.num_layers - 1):
-            w = self.weights[i]
-            u = paddle.matmul(u, w)
-            b = self.biases[i]
-            u = paddle.add(u, b)
+            u = paddle.matmul(u, self.weights[i])
+            u = paddle.add(u, self.biases[i])
             u = self.activation(u)
         u = paddle.matmul(u, self.weights[-1])
         u = paddle.add(u, self.biases[-1])
         return u
-
-    def flatten_params(self):
-        flat_vars = list(map(paddle.flatten, self.weights + self.biases))
-        return paddle.flatten(paddle.concat(flat_vars))
-
-    def reconstruct(self, param_data):
-        params = self.weights + self.biases
-        param_sizes = [param.size for param in params]
-        flat_params = paddle.split(param_data, param_sizes)
-        is_biases = [False for _ in self.weights] + [True for _ in self.biases]
-
-        self.weights = []
-        self.biases = []
-        
-        for old_param, flat_param, is_bias in zip(params, flat_params, is_biases):
-            shape = old_param.shape
-            value = paddle.reshape(flat_param, shape)
-            # new_param = self.create_parameter(shape,
-            #                                   dtype=self.dtype,
-            #                                   is_bias=is_bias,
-            #                                   default_initializer=Assign(value))
-            # if is_bias:
-            #     self.biases.append(new_param)
-            # else:
-            #     self.weights.append(new_param)
-            # self.add_parameter(old_param.name.split('.')[-1], new_param)
-            new_param = value
-            if is_bias:
-                self.biases.append(new_param)
-            else:
-                self.weights.append(new_param)
