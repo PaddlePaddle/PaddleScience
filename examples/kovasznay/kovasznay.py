508f7e0249541f26a61c7ed84d86a22abb8d0385
levi131 <limaolin01@baidu.com>
2022-02-09 12:10:28 +0000

    format update

--- examples/kovasznay/kovasznay.py
+++ examples/kovasznay/kovasznay.py
@@ -1,24 +1,39 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import paddlescience as psci
 import numpy as np
-import paddle
 
 Re = 40
-r = Re/2-np.sqrt(Re**2/4+4*np.pi**2)
+r = Re / 2 - np.sqrt(Re**2 / 4 + 4 * np.pi**2)
+
 
 def KovasznayRecSolution(x, y):
+    u = 1 - np.exp(r * x) * np.cos(2 * np.pi * y)
+    v = r / (2 * np.pi) * np.exp(r * x) * np.sin(2 * np.pi * y)
+    p = 1 / 2 - 1 / 2 * np.exp(2 * r * x)
 
-    u = 1 - np.exp(r*x)*np.cos(2*np.pi*y)
-    v = r/(2*np.pi)*np.exp(r*x)*np.sin(2*np.pi*y)
-    p = 1/2-1/2*np.exp(2*r*x)
+    return u, v, p
 
-    return u,v,p
 
 # Generate BC value
 def GenBC(xy, bc_index):
     sol = np.zeros((len(xy), 3)).astype(np.float32)
     bc_value = np.zeros((len(bc_index), 3)).astype(np.float32)
     for i in range(len(xy)):
-        sol[i][0], sol[i][1], sol[i][2] = KovasznayRecSolution(xy[i][0], xy[i][1])
+        sol[i][0], sol[i][1], sol[i][2] = KovasznayRecSolution(xy[i][0],
+                                                               xy[i][1])
 
     for i in range(len(bc_index)):
         bc_value[i][0] = sol[bc_index[i]][0]
@@ -27,9 +42,11 @@ def GenBC(xy, bc_index):
 
     return bc_value
 
-geo = psci.geometry.Rectangular(space_origin=(-0.5, -0.5), space_extent=(1.5, 1.5))
 
-pdes = psci.pde.NavierStokes(nu=1/Re, rho=1.0)
+geo = psci.geometry.Rectangular(
+    space_origin=(-0.5, -0.5), space_extent=(1.5, 1.5))
+
+pdes = psci.pde.NavierStokes(nu=1 / Re, rho=1.0)
 
 pdes, geo = psci.discretize(pdes, geo, space_nsteps=(50, 50))
 
@@ -45,7 +62,7 @@ net = psci.network.FCNet(
     dtype="float32",
     activation='tanh')
 
-loss = psci.loss.L2(pdes=pdes,geo=geo)
+loss = psci.loss.L2(pdes=pdes, geo=geo)
 
 # Algorithm
 algo = psci.algorithm.PINNs(net=net, loss=loss)
