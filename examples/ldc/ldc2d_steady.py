fix ldc norm computation

examples/ldc/ldc2d_steady.py
@@ -24,24 +24,17 @@ paddle.enable_static()
 
 nup = psci.parameter.Parameter('nu')
 
-# geo = psci.geometry.CircleInRectangular(
-#     origin=(-0.05, -0.05),
-#     extent=(0.05, 0.05),
-#     circle_center=(0.0, 0.0),
-#     circle_radius=0.01)
-
 # set geometry and boundary
 geo = psci.geometry.Rectangular(origin=(-0.05, -0.05), extent=(0.05, 0.05))
 
-geo.add_boundary(
-    name="top", criteria=lambda x, y: y == 0.05, normal=(0.0, 1.0))
-geo.add_boundary(name="down", criteria=lambda x, y: y == -0.05)
-geo.add_boundary(name="left", criteria=lambda x, y: x == -0.05)
-geo.add_boundary(name="right", criteria=lambda x, y: x == 0.05)
+geo.add_boundary(name="top", criteria=lambda x, y: abs(y - 0.05) < 1e-5)
+geo.add_boundary(name="down", criteria=lambda x, y: abs(y + 0.05) < 1e-5)
+geo.add_boundary(name="left", criteria=lambda x, y: abs(x + 0.05) < 1e-5)
+geo.add_boundary(name="right", criteria=lambda x, y: abs(x - 0.05) < 1e-5)
 
 # N-S
 pde = psci.pde.NavierStokes(
-    nu=0.01, rho=1.0, dim=2, time_dependent=False, weight=0.01)
+    nu=0.01, rho=1.0, dim=2, time_dependent=False, weight=0.0001)
 
 # set bounday condition
 weight_top_u = lambda x, y: 1.0 - 20.0 * abs(x)
@@ -63,7 +56,8 @@ pde.add_bc("left", bc_left_u, bc_left_v)
 pde.add_bc("right", bc_right_u, bc_right_v)
 
 # discretization
-pde_disc = psci.discretize(pde, space_npoints=10201, space_method="uniform")
+npoints = 10201
+pde_disc = psci.discretize(pde, space_npoints=npoints, space_method="uniform")
 
 # Network
 # TODO: remove num_ins and num_outs
@@ -71,7 +65,7 @@ net = psci.network.FCNet(
     num_ins=2, num_outs=3, num_layers=10, hidden_size=50, activation='tanh')
 
 # Loss
-loss = psci.loss.L2()
+loss = psci.loss.L2(p=2)
 
 # Algorithm
 algo = psci.algorithm.PINNs(net=net, loss=loss)
@@ -81,74 +75,22 @@ opt = psci.optimizer.Adam(learning_rate=0.001, parameters=net.parameters())
 
 # Solver
 solver = psci.solver.Solver(pde=pde_disc, algo=algo, opt=opt)
-solution = solver.solve(num_epoch=5000)
+solution = solver.solve(num_epoch=10)
 
 psci.visu.save_vtk(geo_disc=pde_disc.geometry, data=solution)
 
-# Predict
+# # MSE
+# # TODO: solution array to dict: interior, bc
+# cord = pde_disc.geometry.interior
+# ref = ref_sol(cord[:, 0], cord[:, 1])
+# mse2 = np.linalg.norm(solution[0][:, 0] - ref, ord=2)**2
 
-# print(pde.bc)
+# n = 1
+# for cord in pde_disc.geometry.boundary.values():
+#     ref = ref_sol(cord[:, 0], cord[:, 1])
+#     mse2 += np.linalg.norm(solution[n][:, 0] - ref, ord=2)**2
+#     n += 1
 
-# Discretization
-# pde_disc = psci.discretize(pde, space_npoints=(11, 11))
+# mse = mse2 / npoints
 
-# # Generate BC weight
-# def GenBCWeight(xy, bc_index):
-#     bc_weight = np.zeros((len(bc_index), 2)).astype(np.float32)
-#     for i in range(len(bc_index)):
-#         id = bc_index[i]
-#         if abs(xy[id][1] - 0.05) < 1e-4:
-#             bc_weight[i][0] = 1.0 - 20 * abs(xy[id][0])
-#             bc_weight[i][1] = 1.0
-#         else:
-#             bc_weight[i][0] = 1.0
-#             bc_weight[i][1] = 1.0
-#     return bc_weight
-
-# # Discretization
-# pdes, geo = psci.discretize(pdes, geo, space_nsteps=(101, 101))
-
-# # bc value
-# bc_value = GenBC(geo.get_space_domain(), geo.get_bc_index())
-# pdes.set_bc_value(bc_value=bc_value, bc_check_dim=[0, 1])
-
-# # Use solution
-# rslt = solution(geo)
-# u = rslt[:, 0]
-# v = rslt[:, 1]
-# u_and_v = np.sqrt(u * u + v * v)
-# psci.visu.save_vtk(geo, u, filename="rslt_u")
-# psci.visu.save_vtk(geo, v, filename="rslt_v")
-# psci.visu.save_vtk(geo, u_and_v, filename="u_and_v")
-
-# openfoam_u = np.load("./openfoam/openfoam_u_100.npy")
-# diff_u = u - openfoam_u
-# RSE_u = np.linalg.norm(diff_u, ord=2)
-# MSE_u = RSE_u * RSE_u / geo.get_domain_size()
-# print("MSE_u: ", MSE_u)
-# openfoam_v = np.load("./openfoam/openfoam_v_100.npy")
-# diff_v = v - openfoam_v
-# RSE_v = np.linalg.norm(diff_v, ord=2)
-# MSE_v = RSE_v * RSE_v / geo.get_domain_size()
-# print("MSE_v: ", MSE_v)
-
-# # Infer with another geometry
-# geo_1 = psci.geometry.Rectangular(
-#     space_origin=(-0.05, -0.05), space_extent=(0.05, 0.05))
-# geo_1 = geo_1.discretize(space_nsteps=(401, 401))
-# rslt_1 = solution(geo_1)
-# u_1 = rslt_1[:, 0]
-# v_1 = rslt_1[:, 1]
-# psci.visu.save_vtk(geo_1, u_1, filename="rslt_u_400")
-# psci.visu.save_vtk(geo_1, v_1, filename="rslt_v_400")
-
-# openfoam_u_1 = np.load("./openfoam/openfoam_u_400.npy")
-# diff_u_1 = u_1 - openfoam_u_1
-# RSE_u_1 = np.linalg.norm(diff_u_1, ord=2)
-# MSE_u_1 = RSE_u_1 * RSE_u_1 / geo_1.get_domain_size()
-# print("MSE_u_400: ", MSE_u_1)
-# openfoam_v_1 = np.load("./openfoam/openfoam_v_400.npy")
-# diff_v_1 = v_1 - openfoam_v_1
-# RSE_v_1 = np.linalg.norm(diff_v_1, ord=2)
-# MSE_v_1 = RSE_v_1 * RSE_v_1 / geo_1.get_domain_size()
-# print("MSE_v_400: ", MSE_v_1)
+# print("MSE is: ", mse)

paddlescience/algorithm/algorithm_pinns.py
@@ -211,8 +211,6 @@ class PINNs(AlgorithmBase):
         labels = inputs_labels[ninputs::]  # labels is the rest
 
         n = 0
-        loss = 0.0
-
         # interior outputs and loss
         loss_eq = 0.0
         for name_i, input_attr in inputs_attr["interior"].items():
@@ -229,7 +227,6 @@ class PINNs(AlgorithmBase):
             loss_eq += loss_i
             outs.append(out_i)
             n += 1
-        loss += paddle.sqrt(loss_eq)
 
         # boundary outputs and loss
         loss_bc = 0.0
@@ -247,9 +244,6 @@ class PINNs(AlgorithmBase):
             loss_bc += loss_b
             outs.append(out_b)
             n += 1
-        loss += paddle.sqrt(loss_bc)
-
-        # print("loss bc: ", loss_bc)
 
         # ic loss
         loss_ic = 0.0
@@ -267,17 +261,33 @@ class PINNs(AlgorithmBase):
             loss_ic += loss_it
             outs.append(out_it)
             n += 1
-            loss += paddle.sqrt(loss_ic)  # TODO: multiple ic
 
         # data loss
+        loss_data = 0.0
         for name_d, input_attr in inputs_attr["data"].items():
             input = inputs[n]
             loss_d, out_d = self.loss.data_loss(
                 pde, self.net, input, input_attr, labels, labels_attr,
                 bs=-1)  # TODO: bs is not used
-            loss += loss_d
+            loss_data += loss_d
             outs.append(out_d)
             n += 1
+
+        # print("loss eq:   ", loss_eq)
+        # print("loss bc:   ", loss_bc)
+        # print("loss ic:   ", loss_ic)
+        # print("loss data: ", loss_data)
+
+        # loss
+        p = self.loss.norm_p
+        if p == 1:
+            loss = self.__sqrt(loss_eq) + self.__sqrt(loss_bc) + self.__sqrt(
+                loss_ic) + self.__sqrt(loss_data)
+        elif p == 2:
+            loss = self.__sqrt(loss_eq + loss_bc + loss_ic + loss_data)
+        else:
+            pass
+            # TODO: error out
 
         return loss, outs  # TODO: return more
 
@@ -289,3 +299,9 @@ class PINNs(AlgorithmBase):
         space_r = np.tile(space, (nt, 1)).reshape((nt * ns, 2))
         timespace = np.concatenate([time_r, space_r], axis=1)
         return timespace
+
+    def __sqrt(self, x):
+        if np.isscalar(x):
+            return np.sqrt(x)
+        else:
+            return paddle.sqrt(x)

paddlescience/loss/loss_L2.py
@@ -39,8 +39,8 @@ class L2(LossBase):
         >>> net = psci.loss.L2(pdes=pdes, geo=geo)
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, p=1):
+        self.norm_p = p
 
     # compute loss on one interior 
     # there are multiple pde
@@ -69,9 +69,9 @@ class L2(LossBase):
             if wgt_eq is None:
                 wgt = None
             elif type(wgt_eq) == LabelInt:
-                wgt = paddle.sqrt(labels[wgt_eq])
+                wgt = labels[wgt_eq]
             elif np.isscalar(wgt_eq):
-                wgt = np.sqrt(wgt_eq)
+                wgt = wgt_eq
             else:
                 pass
                 # TODO: error out
@@ -86,6 +86,12 @@ class L2(LossBase):
                     loss += paddle.norm((rst - rhs)**2, p=1)
                 else:
                     loss += paddle.norm((rst - rhs)**2 * wgt, p=1)
+
+            # print("rst: ", rst)
+            # print("rhs: ", rhs)
+            # print("wgt: ", wgt)
+            # print("loss: ", loss)
+            # print("")
 
         return loss, cmploss.outs
 
@@ -117,9 +123,9 @@ class L2(LossBase):
             if wgt_b is None:
                 wgt = None
             elif type(wgt_b) == LabelInt:
-                wgt = paddle.sqrt(labels[wgt_b])
+                wgt = labels[wgt_b]
             else:
-                wgt = np.sqrt(wgt_b)
+                wgt = wgt_b
 
             if rhs is None:
                 if wgt is None: