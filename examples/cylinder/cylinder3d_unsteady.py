f3f36de50876a3170d78f240c818bf75236fe1a7
Liu-xiandong <85323580+Liu-xiandong@users.noreply.github.com>
2022-05-16 10:57:05 +0800

    Update cylinder3d_unsteady.py

--- examples/cylinder/cylinder3d_unsteady.py
+++ examples/cylinder/cylinder3d_unsteady.py
@@ -17,6 +17,9 @@
 import paddlescience as psci
 import numpy as np
 import paddle
+import os
+import wget
+import zipfile
 
 paddle.seed(1)
 np.random.seed(1)
@@ -24,11 +27,9 @@ np.random.seed(1)
 # paddle.enable_static()
 paddle.disable_static()
 
-
 # load real data
 def GetRealPhyInfo(time, need_cord=False, need_physic=False):
-    real_data = np.load("flow_unsteady_re200/flow_re200_" + str(time) +
-                        "_xyzuvwp.npy")
+    real_data = np.load("openfoam_cylinder_re100/flow_re100_" + str(time) + "_xyzuvwp.npy")
     real_data = real_data.astype(np.float32)
     if need_cord is False and need_physic is False:
         print("Error: you need to get cord or get physic infomation")
@@ -42,15 +43,12 @@ def GetRealPhyInfo(time, need_cord=False, need_physic=False):
     else:
         pass
 
-
-# get init physic infomation
-def GenInitPhyInfo(xyz):
-    uvw = np.zeros((len(xyz), 3)).astype(np.float32)
-    for i in range(len(xyz)):
-        if abs(xyz[i][0] - (-8)) < 1e-4:
-            uvw[i][0] = 1.0
-    return uvw
-
+# get the related real dataset 
+if os.path.exists('./openfoam_cylinder_re100') == False:
+    data_set = 'https://dataset.bj.bcebos.com/PaddleScience/cylinder3D/openfoam_cylinder_re100/cylinder3d_openfoam_re100.zip'
+    wget.download(data_set)
+    with zipfile.ZipFile('cylinder3d_openfoam_re100.zip', 'r') as zip_ref:
+        zip_ref.extractall('openfoam_cylinder_re100')
 
 # define start time and time step
 start_time = 100
@@ -59,7 +57,10 @@ time_step = 1
 cc = (0.0, 0.0)
 cr = 0.5
 geo = psci.geometry.CylinderInCube(
-    origin=(-8, -8, -2), extent=(25, 8, 2), circle_center=cc, circle_radius=cr)
+    origin=(-8, -8, -2),
+    extent=(25, 8, 2),
+    circle_center=cc,
+    circle_radius=cr)
 
 geo.add_boundary(name="left", criteria=lambda x, y, z: abs(x + 8.0) < 1e-4)
 geo.add_boundary(name="right", criteria=lambda x, y, z: abs(x - 25.0) < 1e-4)
@@ -68,7 +69,7 @@ geo.add_boundary(
     criteria=lambda x, y, z: ((x - cc[0])**2 + (y - cc[1])**2 - cr**2) < 1e-4)
 
 # discretize geometry
-geo_disc = geo.discretize(npoints=[100, 100, 4], method="uniform")
+geo_disc = geo.discretize(npoints=[200, 50, 4], method="uniform")
 
 # the real_cord need to be added in geo_disc
 real_cord = GetRealPhyInfo(start_time, need_cord=True)
@@ -84,7 +85,7 @@ pde = psci.pde.NavierStokes(
 
 pde.set_time_interval([100.0, 110.0])
 
-# boundary condition on left side: u=10, v=w=0
+# boundary condition on left side: u=1, v=w=0
 bc_left_u = psci.bc.Dirichlet('u', rhs=1.0, weight=1.0)
 bc_left_v = psci.bc.Dirichlet('v', rhs=0.0, weight=1.0)
 bc_left_w = psci.bc.Dirichlet('w', rhs=0.0, weight=1.0)
@@ -127,22 +128,18 @@ train_epoch = 2000
 
 # Solver time: (100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110]
 num_time_step = 10
-current_interior = np.zeros(
-    (len(pde_disc.geometry.interior), 3)).astype(np.float32)
+current_interior = np.zeros((len(pde_disc.geometry.interior), 3)).astype(np.float32)
 current_user = GetRealPhyInfo(start_time, need_physic=True)[:, 0:3]
 for i in range(num_time_step):
     next_time = start_time + (i + 1) * time_step
-    print("############# train next time=%f train task ############" %
-          next_time)
+    print("### train next time=%f train task ###" % next_time)
     solver.feed_data_interior_cur(current_interior)  # add u(n) interior
     solver.feed_data_user_cur(current_user)  # add u(n) user 
-    solver.feed_data_user_next(GetRealPhyInfo(
-        next_time, need_physic=True))  # add u(n+1) user
-    next_uvwp = solver.solve(num_epoch=train_epoch)
+    solver.feed_data_user_next(GetRealPhyInfo(next_time, need_physic=True))  # add u(n+1) user
+    next_uvwp = solver.solve(num_epoch = train_epoch)
     # Save vtk
-    file_path = "train_flow_unsteady_re200/fac3d_train_rslt_" + str(next_time)
-    psci.visu.save_vtk(
-        filename=file_path, geo_disc=pde_disc.geometry, data=next_uvwp)
+    file_path = "train_cylinder_unsteady_re100/cylinder3d_train_rslt_" + str(next_time)
+    psci.visu.save_vtk(filename=file_path, geo_disc=pde_disc.geometry, data=next_uvwp)
     # next_info -> current_info
     next_interior = np.array(next_uvwp[0])
     next_user = np.array(next_uvwp[-1])
