c0a18deec7630e3b5d7a0ea7ab466fb64f4dc8e5
Hui Xiang <xianghui01@baidu.com>
2022-05-17 05:35:14 +0000

    MV examples/cylinder/* to examples/cylinder/2d_unsteady_continuous/*.

--- examples/cylinder/2d_unsteady_continuous/cylinder2d_unsteady_predict.py
+++ examples/cylinder/2d_unsteady_continuous/cylinder2d_unsteady_predict.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """
 Created in May. 2022
 @author: Hui Xiang, Yanbo Zhang, Shengze Cai
@@ -28,18 +27,19 @@ import paddle.distributed as dist
 from pyevtk.hl import pointsToVTK
 
 import loading_cfd_data as cfd
-from module.cfd import pinn_solver as psolver
+import paddlescience.module.cfd.pinn_solver as psolver
 
 
-def predict(net_params=None, vtk_filename = './vtk/uvp_t_'):
+def predict(net_params=None, vtk_filename='./vtk/uvp_t_'):
     # Loading checkpoint state_dict params
     net_params = net_params
 
     PINN = psolver.PysicsInformedNeuralNetwork(layers=6, net_params=net_params)
 
     # Loading data from openfoam
-    path = './data_0430/'
-    dataloader = cfd.DataLoader(path=path, N_f=100000, time_start=1, time_end=50, time_nsteps=50)
+    path = './datasets/'
+    dataloader = cfd.DataLoader(
+        path=path, N_f=100000, time_start=1, time_end=50, time_nsteps=50)
     #training_time_list = dataloader.select_discretized_time(num_time=30)
     training_time_list = dataloader.select_ordered_time(num_time=50)
 
@@ -52,40 +52,50 @@ def predict(net_params=None, vtk_filename = './vtk/uvp_t_'):
     PINN.set_boundary_data(X=boundary_data)
 
     # Set training data, | t, x, y
-    training_data = dataloader.loading_train_inside_domain_data(training_time_list)
+    training_data = dataloader.loading_train_inside_domain_data(
+        training_time_list)
     PINN.set_eq_training_data(X=training_data)
 
     #outlet
     outlet_p, outlet_t, outlet_x, outlet_y = outlet_data
-    num_outlet_data = int(outlet_t.shape[0]/len(training_time_list))
+    num_outlet_data = int(outlet_t.shape[0] / len(training_time_list))
 
     #boundary
     boundary_u, boundary_v, boundary_t, boundary_x, boundary_y = boundary_data
-    num_boundary_data = int(boundary_t.shape[0]/len(training_time_list))
+    num_boundary_data = int(boundary_t.shape[0] / len(training_time_list))
 
     #training
     training_t, training_x, training_y = training_data
-    num_train_data = int(training_t.shape[0]/len(training_time_list))
+    num_train_data = int(training_t.shape[0] / len(training_time_list))
     #start_time = time.perf_counter()
     for i in range(len(training_time_list)):
         t_step = training_time_list[i]
 
         # training
-        t_x = training_x[(i*num_train_data):(i+1)*num_train_data, :].astype('float32')
-        t_y = training_y[(i*num_train_data):(i+1)*num_train_data, :].astype('float32')
-        t_t = training_t[(i*num_train_data):(i+1)*num_train_data, :].astype('float32')
+        t_x = training_x[(i * num_train_data):(i + 1) *
+                         num_train_data, :].astype('float32')
+        t_y = training_y[(i * num_train_data):(i + 1) *
+                         num_train_data, :].astype('float32')
+        t_t = training_t[(i * num_train_data):(i + 1) *
+                         num_train_data, :].astype('float32')
 
         # outlet 
-        o_x = outlet_x[(i*num_outlet_data):(i+1)*num_outlet_data, :].astype('float32')
-        o_y = outlet_y[(i*num_outlet_data):(i+1)*num_outlet_data, :].astype('float32')
-        o_t = outlet_t[(i*num_outlet_data):(i+1)*num_outlet_data, :].astype('float32')
+        o_x = outlet_x[(i * num_outlet_data):(i + 1) *
+                       num_outlet_data, :].astype('float32')
+        o_y = outlet_y[(i * num_outlet_data):(i + 1) *
+                       num_outlet_data, :].astype('float32')
+        o_t = outlet_t[(i * num_outlet_data):(i + 1) *
+                       num_outlet_data, :].astype('float32')
 
         # boundary 
-        b_x = boundary_x[(i*num_boundary_data):(i+1)*num_boundary_data, :].astype('float32')
-        b_y = boundary_y[(i*num_boundary_data):(i+1)*num_boundary_data, :].astype('float32')
-        b_t = boundary_t[(i*num_boundary_data):(i+1)*num_boundary_data, :].astype('float32')
+        b_x = boundary_x[(i * num_boundary_data):(i + 1) *
+                         num_boundary_data, :].astype('float32')
+        b_y = boundary_y[(i * num_boundary_data):(i + 1) *
+                         num_boundary_data, :].astype('float32')
+        b_t = boundary_t[(i * num_boundary_data):(i + 1) *
+                         num_boundary_data, :].astype('float32')
 
-        all_x = np.concatenate((t_x, o_x, b_x ))
+        all_x = np.concatenate((t_x, o_x, b_x))
         all_y = np.concatenate((t_y, o_y, b_y))
         all_t = np.concatenate((t_t, o_t, b_t))
 
@@ -94,27 +104,37 @@ def predict(net_params=None, vtk_filename = './vtk/uvp_t_'):
         tensor_y = paddle.to_tensor(all_y, dtype='float32')
 
         #start_time = time.perf_counter()
-        u, v, p = PINN.predict(net_params, (tensor_t,tensor_x,tensor_y))
+        u, v, p = PINN.predict(net_params, (tensor_t, tensor_x, tensor_y))
         #stop_time = time.perf_counter()
         #print('Spend %.3f seconds to predict 1 time steps.'%(stop_time - start_time))
         u = u.numpy()
         v = v.numpy()
         p = p.numpy()
 
-        axis_z = np.zeros(num_train_data+num_outlet_data+num_boundary_data, dtype="float32")
+        axis_z = np.zeros(
+            num_train_data + num_outlet_data + num_boundary_data,
+            dtype="float32")
         filename = vtk_filename + str(t_step)
-        pointsToVTK(filename, all_x.flatten().copy(), all_y.flatten().copy(), axis_z.flatten().copy(),
-                    data={"u": u.copy(), "v":v.copy(), "p": p.copy()})        
+        pointsToVTK(
+            filename,
+            all_x.flatten().copy(),
+            all_y.flatten().copy(),
+            axis_z.flatten().copy(),
+            data={"u": u.copy(),
+                  "v": v.copy(),
+                  "p": p.copy()})
 
-def predict_once_for_all(net_params=None, vtk_filename = './vtk/uvp_t_'):
+
+def predict_once_for_all(net_params=None, vtk_filename='./vtk/uvp_t_'):
     # Loading checkpoint state_dict params
     net_params = net_params
 
     PINN = psolver.PysicsInformedNeuralNetwork(layers=6, net_params=net_params)
 
     # Loading data from openfoam
-    path = './data_0430/'
-    dataloader = cfd.DataLoader(path=path, N_f=100000, time_start=1, time_end=50, time_nsteps=50)
+    path = './datasets/'
+    dataloader = cfd.DataLoader(
+        path=path, N_f=100000, time_start=1, time_end=50, time_nsteps=50)
     #training_time_list = dataloader.select_discretized_time(num_time=30)
     training_time_list = dataloader.select_ordered_time(num_time=50)
 
@@ -127,22 +147,23 @@ def predict_once_for_all(net_params=None, vtk_filename = './vtk/uvp_t_'):
     PINN.set_boundary_data(X=boundary_data)
 
     # Set training data, | t, x, y
-    training_data = dataloader.loading_train_inside_domain_data(training_time_list)
+    training_data = dataloader.loading_train_inside_domain_data(
+        training_time_list)
     PINN.set_eq_training_data(X=training_data)
 
     #outlet
     outlet_p, outlet_t, outlet_x, outlet_y = outlet_data
-    num_outlet_data = int(outlet_t.shape[0]/len(training_time_list))
+    num_outlet_data = int(outlet_t.shape[0] / len(training_time_list))
 
     #boundary
     boundary_u, boundary_v, boundary_t, boundary_x, boundary_y = boundary_data
-    num_boundary_data = int(boundary_t.shape[0]/len(training_time_list))
+    num_boundary_data = int(boundary_t.shape[0] / len(training_time_list))
 
     #training
     training_t, training_x, training_y = training_data
-    num_train_data = int(training_t.shape[0]/len(training_time_list))
+    num_train_data = int(training_t.shape[0] / len(training_time_list))
     #start_time = time.perf_counter()
-        # training
+    # training
     t_x = training_x[:, :].astype('float32')
     t_y = training_y[:, :].astype('float32')
     t_t = training_t[:, :].astype('float32')
@@ -157,7 +178,7 @@ def predict_once_for_all(net_params=None, vtk_filename = './vtk/uvp_t_'):
     b_y = boundary_y[:, :].astype('float32')
     b_t = boundary_t[:, :].astype('float32')
 
-    all_x = np.concatenate((t_x, o_x, b_x ))
+    all_x = np.concatenate((t_x, o_x, b_x))
     all_y = np.concatenate((t_y, o_y, b_y))
     all_t = np.concatenate((t_t, o_t, b_t))
 
@@ -166,29 +187,33 @@ def predict_once_for_all(net_params=None, vtk_filename = './vtk/uvp_t_'):
     tensor_y = paddle.to_tensor(all_y, dtype='float32')
 
     # start_time = time.perf_counter()
-    u, v, p = PINN.predict(net_params, (tensor_t,tensor_x,tensor_y))
+    u, v, p = PINN.predict(net_params, (tensor_t, tensor_x, tensor_y))
     # stop_time = time.perf_counter()
     # print('Spend %.3f seconds to predict 50 time steps.'%(stop_time - start_time))
     u = u.numpy()
     v = v.numpy()
     p = p.numpy()
 
-    total_len = num_train_data+num_outlet_data+num_boundary_data
+    total_len = num_train_data + num_outlet_data + num_boundary_data
     for i in range(len(training_time_list)):
         t_step = training_time_list[i]
-        axis_z = np.zeros(num_train_data+num_outlet_data+num_boundary_data, dtype="float32")
+        axis_z = np.zeros(
+            num_train_data + num_outlet_data + num_boundary_data,
+            dtype="float32")
         filename = vtk_filename + str(t_step)
-        pointsToVTK(filename, all_x[i*total_len:(i+1)*total_len,:].flatten().copy(), 
-                    all_y[i*total_len:(i+1)*total_len,:].flatten().copy(), axis_z.flatten().copy(),
-                    data={"u": u[i*total_len:(i+1)*total_len].copy(), 
-                          "v": v[i*total_len:(i+1)*total_len].copy(), 
-                          "p": p[i*total_len:(i+1)*total_len].copy()})        
+        pointsToVTK(
+            filename,
+            all_x[i * total_len:(i + 1) * total_len, :].flatten().copy(),
+            all_y[i * total_len:(i + 1) * total_len, :].flatten().copy(),
+            axis_z.flatten().copy(),
+            data={
+                "u": u[i * total_len:(i + 1) * total_len].copy(),
+                "v": v[i * total_len:(i + 1) * total_len].copy(),
+                "p": p[i * total_len:(i + 1) * total_len].copy()
+            })
+
 
 if __name__ == "__main__":
     net_params = './checkpoint/pretrained_net_params'
     vtk_filename = './vtk/uvp_t_'
     predict_once_for_all(net_params=net_params, vtk_filename=vtk_filename)
-
-
-
-
