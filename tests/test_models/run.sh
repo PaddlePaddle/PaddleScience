b14cbbfc61efb49921d5283fb07041689f7db9c9
shjNT <sunhongjiang@baidu.com>
2022-05-30 09:42:07 +0800

    tmporary serial exec environment

--- tests/test_models/run.sh
+++ tests/test_models/run.sh
@@ -21,6 +21,7 @@ serial_bug=0
 distributed_bug=0
 bug=0
 
+CUDA_VISIBLE_DEVICES=0
 echo "===== examples bug list =====" >  result.txt
 echo "serial bug list:" >>  result.txt
 for file in ${cases}
@@ -40,6 +41,7 @@ done
 echo "serial bugs: "${serial_bug} >> result.txt
 
 
+CUDA_VISIBLE_DEVICES=0,1
 ignore=""
 echo "distributed bug list:" >>  result.txt
 for file in ${cases}
