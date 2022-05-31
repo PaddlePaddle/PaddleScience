61d1e7085c4b1420b2a4d953e54888a08cd16347
Liu-xiandong <liuxiandong@ncic.ac.cn>
2022-05-31 03:07:27 +0000

    check result of static and dynamic graph

--- examples/cylinder/3d_unsteady_discrete/baseline/cylinder3d_unsteady_predict.py
+++ examples/cylinder/3d_unsteady_discrete/baseline/cylinder3d_unsteady_predict.py
@@ -25,7 +25,28 @@ paddle.seed(1)
 np.random.seed(1)
 
 paddle.enable_static()
+
 #paddle.disable_static()
+
+
+# load real data 
+def GetRealPhyInfo(time, need_info=None):
+    # if real data don't exist, you need to download it.
+    if os.path.exists('./openfoam_cylinder_re100') == False:
+        data_set = 'https://dataset.bj.bcebos.com/PaddleScience/cylinder3D/openfoam_cylinder_re100/cylinder3d_openfoam_re100.zip'
+        wget.download(data_set)
+        with zipfile.ZipFile('cylinder3d_openfoam_re100.zip', 'r') as zip_ref:
+            zip_ref.extractall('openfoam_cylinder_re100')
+    real_data = np.load("openfoam_cylinder_re100/flow_re100_" + str(
+        int(time)) + "_xyzuvwp.npy")
+    real_data = real_data.astype(np.float32)
+    if need_info == 'cord':
+        return real_data[:, 0:3]
+    elif need_info == 'physic':
+        return real_data[:, 3:7]
+    else:
+        return real_data
+
 
 # define start time
 start_time = 100
@@ -35,17 +56,11 @@ cr = 0.5
 geo = psci.geometry.CylinderInCube(
     origin=(-8, -8, -2), extent=(25, 8, 2), circle_center=cc, circle_radius=cr)
 
-geo.add_boundary(name="left", criteria=lambda x, y, z: abs(x + 8.0) < 1e-4)
-geo.add_boundary(name="right", criteria=lambda x, y, z: abs(x - 25.0) < 1e-4)
-geo.add_boundary(
-    name="circle",
-    criteria=lambda x, y, z: ((x - cc[0])**2 + (y - cc[1])**2 - cr**2) < 1e-4)
-
 # discretize geometry
 geo_disc = geo.discretize(npoints=[200, 50, 4], method="uniform")
 
 # the real_cord need to be added in geo_disc
-# geo_disc.user = GetRealPhyInfo(start_time, need_info='cord')
+geo_disc.user = GetRealPhyInfo(start_time, need_info='cord')
 
 # N-S equation
 pde = psci.pde.NavierStokes(
@@ -56,24 +71,6 @@ pde = psci.pde.NavierStokes(
     weight=[0.01, 0.01, 0.01, 0.01])
 
 pde.set_time_interval([100.0, 110.0])
-
-# boundary condition on left side: u=1, v=w=0
-bc_left_u = psci.bc.Dirichlet('u', rhs=1.0, weight=1.0)
-bc_left_v = psci.bc.Dirichlet('v', rhs=0.0, weight=1.0)
-bc_left_w = psci.bc.Dirichlet('w', rhs=0.0, weight=1.0)
-
-# boundary condition on right side: p=0
-bc_right_p = psci.bc.Dirichlet('p', rhs=0.0, weight=1.0)
-
-# boundary on circle
-bc_circle_u = psci.bc.Dirichlet('u', rhs=0.0, weight=1.0)
-bc_circle_v = psci.bc.Dirichlet('v', rhs=0.0, weight=1.0)
-bc_circle_w = psci.bc.Dirichlet('w', rhs=0.0, weight=1.0)
-
-# add bounday and boundary condition
-pde.add_bc("left", bc_left_u, bc_left_v, bc_left_w)
-pde.add_bc("right", bc_right_p)
-pde.add_bc("circle", bc_circle_u, bc_circle_v, bc_circle_w)
 
 # pde discretization 
 pde_disc = pde.discretize(
@@ -100,16 +97,24 @@ if paddle.in_dynamic_mode():
     next_uvwp = solver.predict(
         dynamic_net_file='checkpoint/dynamic_net_params_1000.pdparams',
         dynamic_opt_file='checkpoint/dynamic_opt_params_1000.pdopt')
+    # # check net
+    # for name, param in solver.algo.net.named_parameters():
+    #     print(name)
+    #     print(param)
 else:
     next_uvwp = solver.predict(
-        static_model_file='checkpoint/dynamic_net_params_1000.pdparams')
+        static_model_file='checkpoint/static_model_params_1000.pdparams')
 
 # save vtk
-file_path = "predict_cylinder_unsteady_re100/rslt_" + str(100)
+if paddle.in_dynamic_mode():
+    file_path = "predict_cylinder_unsteady_re100/rslt_dynamic_" + str(100)
+else:
+    file_path = "predict_cylinder_unsteady_re100/rslt_static_" + str(100)
 psci.visu.save_vtk(
     filename=file_path, geo_disc=pde_disc.geometry, data=next_uvwp)
 
-# predict time: (100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110]
-# for next_time in range(111, 121):
-# current_time -> next time
-# current_interior = np.array(next_uvwp[0])[:, 0:3]
+# save npy
+result = next_uvwp[-1]
+result = np.array(result)
+print(result[0:20, :])
+np.save("predict_cylinder_unsteady_re100/predict_result.npy", result)
