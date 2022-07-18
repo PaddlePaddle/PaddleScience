03765f15693a029f2aa4c099352bd7734672b7f6
Liu-xiandong <85323580+Liu-xiandong@users.noreply.github.com>
2022-07-18 15:23:10 +0800

    Update ldc2d_steady_train.py

--- examples/ldc/ldc2d_steady_train.py
+++ examples/ldc/ldc2d_steady_train.py
@@ -71,7 +71,7 @@ opt = psci.optimizer.Adam(learning_rate=0.001, parameters=net.parameters())
 
 # Solver
 solver = psci.solver.Solver(pde=pde_disc, algo=algo, opt=opt)
-solution = solver.solve(num_epoch=20)
+solution = solver.solve(num_epoch=20000)
 
 solution = solver.predict()
 
