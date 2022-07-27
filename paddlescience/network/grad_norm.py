126a274b3e79c5841d072af8b73ddcca2e7087ee
Asthestarsfalll <1186454801@qq.com>
2022-07-27 19:51:25 +0800

    add GradNorm without test

---
+++ paddlescience/network/grad_norm.py
@@ -0,0 +1,88 @@
+import numpy as np
+import paddle
+import paddle.nn
+from paddle.nn.initializer import Assign
+from .network_base import NetworkBase
+
+
+class GradNorm(NetworkBase):
+    r"""
+    Gradient normalization for adaptive loss balancing.
+    Parameters:
+        net(NetworkBase): The network which must have "get_shared_layer" method.
+        n_loss(int): The number of loss, must be greater than 1.
+        alpha(float): The hyperparameter which controls learning rate, must be greater than 0.
+        weight_attr(list, tuple): The inital weights for "loss_weights". If not specified, "loss_weights" will be initialized with 1.
+    """
+
+    def __init__(self, net, n_loss, alpha, weight_attr=None):
+        super().__init__()
+        if not isinstance(net, NetworkBase):
+            raise ValueError("'net' must be a NetworkBase subclass instance.")
+        if not hasattr(net, 'get_shared_layer'):
+            raise ValueError("'net' must be must have 'get_shared_layer' method.")
+        if n_loss <= 1:
+            raise ValueError("'n_loss' must be greater than 1, but got {}".format(n_loss))
+        if alpha < 0:
+            raise ValueError("'alpha' is a positive number, but got {}".format(alpha))
+        if weight_attr is not None:
+            if len(weight_attr) != n_loss:
+                raise ValueError("weight_attr must have same length with loss weights.")
+
+        self.net = net
+        self.loss_weights = self.create_parameter(
+            shape=[n_loss], 
+            attr=Assign(weight_attr if weight_attr else [1] * n_loss),
+            dtype=self._dtype, is_bias=False)
+        self.set_grad()
+        self.alpha = float(alpha)
+        self.initial_losses = None
+
+    def nn_func(self, ins):
+        return self.net.nn_func(ins)
+
+    def __getattr__(self, __name):
+        try:
+            return super().__getattr__(__name)
+        except:
+            return getattr(self.net, __name)
+
+    def get_grad_norm_loss(self, losses):
+        if isinstance(losses, list):
+            losses = paddle.concat(losses)
+
+        if self.initial_losses is None:
+            self.initial_losses = losses.numpy()
+
+        W = self.net.get_last_shared_layer()
+
+        if self.loss_weights.grad is not None:
+            self.loss_weights.grad.set_value(paddle.zeros_like(self.loss_weights))
+
+        norms = []
+        for i in range(losses.shape[0]):
+            grad = paddle.autograd.grad(losses[i], W, retain_graph=True)
+            norms.append(paddle.norm(self.loss_weights[i]*grad[0], p=2))
+        norms = paddle.concat(norms)
+
+        loss_ratio = losses.numpy() / self.initial_losses
+        inverse_train_rate = loss_ratio / np.mean(loss_ratio)
+
+        mean_norm = np.mean(norms.numpy())
+        constant_term = paddle.to_tensor(mean_norm * np.power(inverse_train_rate, self.alpha), dtype=self._dtype)
+        grad_norm_loss = paddle.norm(norms - constant_term, p=1)
+        self.loss_weights.grad.set_value(paddle.autograd.grad(grad_norm_loss, self.loss_weights)[0])
+
+        return grad_norm_loss
+
+    def reset_initial_losses(self):
+        self.initial_losses = None
+
+    def set_grad(self):
+        x = paddle.ones_like(self.loss_weights)
+        x *= self.loss_weights
+        x.backward()
+        self.loss_weights.grad.set_value(paddle.zeros_like(self.loss_weights))
+
+    def get_weights(self):
+        return self.loss_weights.numpy()
