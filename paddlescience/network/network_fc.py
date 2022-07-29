30aa0cbb840aaef74e49487259ec457ad6e3cb43
xingfeng01 <xingfeng@baidu.com>
2022-07-29 02:51:05 +0000

    update func name nn_func

--- paddlescience/network/network_fc.py
+++ paddlescience/network/network_fc.py
@@ -110,7 +110,7 @@ class FCNet(NetworkBase):
             self.add_parameter("w_" + str(i), w)
             self.add_parameter("b_" + str(i), b)
 
-    def nn_func_paddle(self, ins):
+    def __nn_func_paddle(self, ins):
         u = ins
         for i in range(self.num_layers - 1):
             u = paddle.matmul(u, self._weights[i])
@@ -121,7 +121,7 @@ class FCNet(NetworkBase):
         return u
 
     def nn_func(self, ins, params=None):
-        return self.nn_func_paddle(ins)
+        return self.__nn_func_paddle(ins)
 
     def __call__(self, input):
         return NetOut(self, input)
