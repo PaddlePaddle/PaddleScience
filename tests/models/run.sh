47e0be49507be7cd3cc7edef36abc7071f7e6186
shjNT <sunhongjiang@baidu.com>
2022-08-05 19:41:29 +0800

    add run

---
+++ tests/models/run.sh
@@ -0,0 +1,35 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+rm -rf test_*.py
+cases=`find ../config/$1 -name "*.yaml" | sort`
+
+echo $cases
+ignoe=""
+bug=0
+
+echo "============ failed cases =============" >> result.txt
+for file_dir in ${cases}
+do
+    name=`basename -s .yaml $file_dir`
+    echo ${name}
+    python3.7 generate.py -f ${name}
+    python3.7 test_${name}.py
+    if [ $? -ne 0 ]; then
+        echo ${file} >> result.txt
+        bug=`expr ${bug} + 1`
+    fi
+done
+echo "total bugs: "${bug} >> result.txt
+exit ${bug}
