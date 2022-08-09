b77ac306be07eaa684b9e3ed4eda3d139d4893ab
xingfeng01 <xingfeng@baidu.com>
2022-08-09 10:31:28 +0000

    four-order eq

---
+++ paddlescience/pde/pde_plate_equilibrium.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .pde_base import PDE
+import sympy
+import numpy as np
+
+__all__ = ['PlateEquilibrium']
+
+
+# PlateEquilibrium equation
+class PlateEquilibrium(PDE):
+    """
+    Plate Equilibrium Equation
+    
+    Parameters:
+        stiff (float): 
+        mass (float): 
+
+    Example:
+        >>> import paddlescience as psci
+        >>> pde = psci.pde.PlateEquilibrium()
+    """
+
+    def __init__(self, stiff, mass, rhs=None, time_dependent=False,
+                 weight=1.0):
+
+        if time_dependent == True:
+
+            t = sympy.Symbol('t')
+            x = sympy.Symbol('x')
+            y = sympy.Symbol('y')
+            w = sympy.Function('w')(t, x, y)
+            super(PlateEquilibrium, self).__init__([t, x, y], [w], weight)
+
+            w4x = w.diff(x).diff(x).diff(x).diff(x)
+            w4y = w.diff(y).diff(y).diff(y).diff(y)
+            w2x2y = w.diff(y).diff(y).diff(x).diff(x)
+            w2t = w.diff(t).diff(t)
+
+            eq = stiff * w4x + 2 * mass * w2x2y + mass * w4y + stiff / mass * w.diff(
+                t).diff(t)
+            self.add_equation(eq, rhs)
+
+        else:
+
+            x = sympy.Symbol('x')
+            y = sympy.Symbol('y')
+            w = sympy.Function('w')(x, y)
+            super(PlateEquilibrium, self).__init__([x, y], [w], weight)
+
+            w4x = w.diff(x).diff(x).diff(x).diff(x)
+            w4y = w.diff(y).diff(y).diff(y).diff(y)
+            w2x2y = w.diff(y).diff(y).diff(x).diff(x)
+
+            eq = stiff * w4x + 2.0 * mass * w2x2y + mass * w4y
+            self.add_equation(eq, rhs)
