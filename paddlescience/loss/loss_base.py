d66f1e083c367be04411a71d2cfda353b033fb92
JiabinYang <360788950@qq.com>
2022-09-26 11:51:47 +0000

    fix p=1 error

--- paddlescience/loss/loss_base.py
+++ paddlescience/loss/loss_base.py
@@ -204,22 +204,7 @@ class CompFormula:
 
 
 def l2_norm_square(x, wgt=None):
-    # new ad
-    if config.prim_enabled():
-        if wgt is None:
-            print("here1")
-            l2_norm = paddle.norm(x, p=2)
-        elif np.isscalar(wgt):
-            print("here2")
-            wgt2 = np.sqrt(wgt)
-            l2_norm = paddle.norm(x * wgt2, p=2)
-        else:
-            print("here3")
-            wgt2 = paddle.sqrt(wgt)
-            l2_norm = paddle.norm(x * wgt2, p=2)
-        return l2_norm * l2_norm
+    if wgt is None:
+        return paddle.norm(x**2, p=1)
     else:
-        if wgt is None:
-            return paddle.norm(x**2, p=1)
-        else:
-            return paddle.norm(x**2 * wgt, p=1)
+        return paddle.norm(x**2 * wgt, p=1)
