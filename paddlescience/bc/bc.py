3e3b0b14de2c17bee75edc99176b847f18ed0804
xingfeng01 <xingfeng@baidu.com>
2022-09-28 07:27:49 +0000

    fix neumann and robin der definition

--- paddlescience/bc/bc.py
+++ paddlescience/bc/bc.py
@@ -87,8 +87,8 @@ class Neumann(BC):
 
     def to_formula(self, indvar):
         n = sympy.Symbol('n')
-        ud = sympy.Function(self.name)(n)
-        self.formula = ud.diff(n)
+        u = sympy.Function(self.name)(*indvar)
+        self.formula = sympy.Derivative(u, n)
 
     def discretize(self, indvar):
         bc_disc = copy.deepcopy(self)
@@ -119,8 +119,7 @@ class Robin(BC):
     def to_formula(self, indvar):
         n = sympy.Symbol('n')
         u = sympy.Function(self.name)(*indvar)
-        ud = sympy.Function(self.name)(n)
-        self.formula = u + ud.diff(n)
+        self.formula = u + sympy.Derivative(u, n)
 
     def discretize(self, indvar):
         bc_disc = copy.deepcopy(self)
