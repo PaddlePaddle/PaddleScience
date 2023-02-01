faff09d5ec1a90ad864e26ea7803e939a1e5da81
HydrogenSulfate <wangguan12>
2023-02-01 03:07:49 +0000

    train ldc2d_steady with new geometry

--- paddlescience/__init__.py
+++ paddlescience/__init__.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ import paddle
 from . import config
 from . import parameter
 from . import geometry
+from . import geometry_new
 from . import bc
 from . import ic
 from . import pde
