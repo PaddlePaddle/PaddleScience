ba7b4f97572b3efea5a100f383f5421a4c5b2048
HydrogenSulfate <490868991@qq.com>
2023-02-22 02:33:27 +0000

    remove open3d and pymesh from requirements

--- paddlescience/neo_geometry/inflation.py
+++ paddlescience/neo_geometry/inflation.py
@@ -1,20 +1,28 @@
 # Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import warnings
+
 import numpy as np
-import open3d
-import pymesh
+
+try:
+    import open3d
+    import pymesh
+except ImportError:
+    warnings.warn(
+        f"Refer to README.md and install open3d and pymesh before using inflation API in neo_geometry"
+    )
 
 
 def inflation(mesh, dis, direction=1):
