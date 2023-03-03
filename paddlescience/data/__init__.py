218f6c7cbd0c13fa25693c53d4f1d5f0ab4603b9
zhiminzhang0830 <452516515@qq.com>
2023-03-03 02:50:10 +0000

    add transformer physx dataset

--- paddlescience/data/__init__.py
+++ paddlescience/data/__init__.py
@@ -11,5 +11,50 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from paddle.io import DataLoader, BatchSampler, DistributedBatchSampler
 
 from .data_process import save_data, load_data
+from .trphysx_dataset import LorenzDataset, CylinderDataset, RosslerDataset
+
+
+def build_dataloader(dataset_name,
+                     mode,
+                     batch_size,
+                     shuffle,
+                     drop_last,
+                     num_workers=8,
+                     dataset_args=dict()):
+    """
+    Build the dataloader according to arguments 
+    dataset_name - name of the dataset 
+    mode - train, val or test, 
+    batch_size - batch size, 
+    shuffle - is shuffle data
+    drop_last -  """
+
+    assert mode in ['train', 'val', 'test']
+
+    assert dataset_name in [
+        'LorenzDataset', 'CylinderDataset', 'RosslerDataset'
+    ]
+    dataset = eval(dataset_name)(**dataset_args)
+
+    if mode == "train":
+        # Distribute data to multiple cards
+        batch_sampler = DistributedBatchSampler(
+            dataset=dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=drop_last)
+    else:
+        # Distribute data to single card
+        batch_sampler = BatchSampler(
+            dataset=dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=drop_last)
+
+    data_loader = DataLoader(
+        dataset=dataset, batch_sampler=batch_sampler, num_workers=num_workers)
+
+    return data_loader
