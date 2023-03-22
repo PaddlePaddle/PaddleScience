3531356349e32c2d67ab79bff96cf12e6296d66f
HydrogenSulfate <490868991@qq.com>
2023-03-22 11:14:23 +0000

    remove useless import

--- ppsci/geometry/pointcloud.py
+++ ppsci/geometry/pointcloud.py
@@ -13,8 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import os.path as osp
-
 import numpy as np
 
 from ppsci.geometry import geometry
