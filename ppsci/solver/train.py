2552dcff45e99d0166c8b3518d71bb42fb38fb09
HydrogenSulfate <490868991@qq.com>
2023-04-26 13:09:46 +0000

    rename label_expr to output_expr for all Constraint class

--- ppsci/solver/train.py
+++ ppsci/solver/train.py
@@ -57,8 +57,8 @@ def train_epoch_func(solver, epoch_id, log_freq):
             evaluator = expression.ExpressionSolver(
                 _constraint.input_keys, _constraint.output_keys, solver.model
             )
-            for label_name, label_formula in _constraint.label_expr.items():
-                evaluator.add_target_expr(label_formula, label_name)
+            for output_name, output_formula in _constraint.output_expr.items():
+                evaluator.add_target_expr(output_formula, output_name)
 
             # forward for every constraint
             if solver.use_amp:
@@ -155,8 +155,8 @@ def train_LBFGS_epoch_func(solver, epoch_id, log_freq):
                 evaluator = expression.ExpressionSolver(
                     _constraint.input_keys, _constraint.output_keys, solver.model
                 )
-                for label_name, label_formula in _constraint.label_expr.items():
-                    evaluator.add_target_expr(label_formula, label_name)
+                for output_name, output_formula in _constraint.output_expr.items():
+                    evaluator.add_target_expr(output_formula, output_name)
 
                 # forward for every constraint
                 output_dict_i = evaluator(input_dict_list[i])
--- ppsci/validate/geo_validator.py
+++ ppsci/validate/geo_validator.py
@@ -49,7 +49,7 @@ class GeometryValidator(base.Validator):
         ... )
         ```
     Args:
-        label_expr (Dict[str, Callable]): Function in dict for computing output.
+        output_expr (Dict[str, Callable]): Function in dict for computing output.
             e.g. {"u_mul_v": lambda out: out["u"] * out["v"]} means the model output u
             will be multiplied by model output v and the result will be named "u_mul_v".
         label_dict (Dict[str, Union[float, Callable]]): Function in dict for computing
@@ -68,7 +68,7 @@ class GeometryValidator(base.Validator):
 
     def __init__(
         self,
-        label_expr: Dict[str, Callable],
+        output_expr: Dict[str, Callable],
         label_dict: Dict[str, Union[float, Callable]],
         geom: geometry.Geometry,
         dataloader_cfg: Dict[str, Any],
@@ -80,10 +80,10 @@ class GeometryValidator(base.Validator):
         with_initial: bool = False,
         name: Optional[str] = None,
     ):
-        self.label_expr = label_expr
-        for label_name, expr in self.label_expr.items():
+        self.output_expr = output_expr
+        for label_name, expr in self.output_expr.items():
             if isinstance(expr, str):
-                self.label_expr[label_name] = sp_parser.parse_expr(expr)
+                self.output_expr[label_name] = sp_parser.parse_expr(expr)
 
         self.label_dict = label_dict
         self.input_keys = geom.dim_keys
