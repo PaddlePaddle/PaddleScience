0e093c59ba244c1b8b8bdb48c021a938a43a2f0f
zhiminzhang0830 <452516515@qq.com>
2023-05-11 07:17:45 +0000

    add import PrecipNet in ppsci/arch/__init__.py

--- ppsci/arch/__init__.py
+++ ppsci/arch/__init__.py
@@ -21,6 +21,7 @@ from ppsci.arch.embedding_koopman import CylinderEmbedding  # isort:skip
 from ppsci.arch.physx_transformer import PhysformerGPT2  # isort:skip
 from ppsci.arch.model_list import ModelList  # isort:skip
 from ppsci.arch.afno import AFNONet  # isort:skip
+from ppsci.arch.afno import PrecipNet  # isort:skip
 from ppsci.utils import logger  # isort:skip
 
 
@@ -32,6 +33,7 @@ __all__ = [
     "PhysformerGPT2",
     "ModelList",
     "AFNONet",
+    "PrecipNet",
     "build_model",
 ]
 
