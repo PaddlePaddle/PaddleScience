54c6c7049d1690a926a454a53fc99b0fea3baca8
HydrogenSulfate <490868991@qq.com>
2023-05-16 12:27:53 +0000

    remove useless imports

--- ppsci/data/dataset/csv_dataset.py
+++ ppsci/data/dataset/csv_dataset.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import types
 from typing import Callable
 from typing import Dict
 from typing import Optional
