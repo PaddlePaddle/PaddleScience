200bde2a4623f9ec1282ee8b4d5cfb3411af5c91
wangguan <772359200@qq.com>
2023-05-16 09:41:44 +0000

    clean code

--- examples/aneurysm/aneurysm_flow_FCNN.py
+++ examples/aneurysm/aneurysm_flow_FCNN.py
@@ -55,8 +55,8 @@ class NavierStokes(ppsci.equation.pde.base.PDE):
             momentum_x = (
                 u * jacobian(u, x)
                 + v * jacobian(u, y)
-                - nu / rho * hessian(u, x)
-                - nu / rho * hessian(u, y)
+                - NU / rho * hessian(u, x)
+                - NU / rho * hessian(u, y)
                 + 1 / rho * jacobian(p, x)
             )
             if self.time:
@@ -66,7 +66,7 @@ class NavierStokes(ppsci.equation.pde.base.PDE):
                 z = out["z"]
                 w = out["w"]
                 momentum_x += w * jacobian(u, z)
-                momentum_x -= nu / rho * hessian(u, z)
+                momentum_x -= NU / rho * hessian(u, z)
             return momentum_x
 
         self.add_equation("momentum_x", momentum_x_compute_func)
@@ -77,8 +77,8 @@ class NavierStokes(ppsci.equation.pde.base.PDE):
             momentum_y = (
                 u * jacobian(v, x)
                 + v * jacobian(v, y)
-                - nu / rho * hessian(v, x)
-                - nu / rho * hessian(v, y)
+                - NU / rho * hessian(v, x)
+                - NU / rho * hessian(v, y)
                 + 1 / rho * jacobian(p, y)
             )
             if self.time:
@@ -88,7 +88,7 @@ class NavierStokes(ppsci.equation.pde.base.PDE):
                 z = out["z"]
                 w = out["w"]
                 momentum_y += w * jacobian(v, z)
-                momentum_y -= nu / rho * hessian(v, z)
+                momentum_y -= NU / rho * hessian(v, z)
             return momentum_y
 
         self.add_equation("momentum_y", momentum_y_compute_func)
@@ -102,9 +102,9 @@ class NavierStokes(ppsci.equation.pde.base.PDE):
                     u * jacobian(w, x)
                     + v * jacobian(w, y)
                     + w * jacobian(w, z)
-                    - nu / rho * hessian(w, x)
-                    - nu / rho * hessian(w, y)
-                    - nu / rho * hessian(w, z)
+                    - NU / rho * hessian(w, x)
+                    - NU / rho * hessian(w, y)
+                    - NU / rho * hessian(w, z)
                     + 1 / rho * jacobian(p, z)
                 )
                 if self.time:
@@ -115,67 +115,93 @@ class NavierStokes(ppsci.equation.pde.base.PDE):
             self.add_equation("momentum_z", momentum_z_compute_func)
 
 
+def predict(
+    input_dict,
+    solver,
+):
+    for key, val in input_dict.items():
+        input_dict[key] = paddle.to_tensor(val, dtype="float32")
+    evaluator = ppsci.utils.expression.ExpressionSolver(
+        input_dict.keys(), ["u", "v", "p"], solver.model
+    )
+    output_expr_dict = {
+        "u": lambda d: d["u"],
+        "v": lambda d: d["v"],
+        "p": lambda d: d["p"],
+    }
+    for output_key, output_expr in output_expr_dict.items():
+        evaluator.add_target_expr(output_expr, output_key)
+    output_dict = evaluator(input_dict)
+    return output_dict
+
+
 if __name__ == "__main__":
     os.chdir("/workspace/wangguan/PaddleScience_Surrogate/examples/aneurysm")
-    output_dir = "./output_0504_debug"
+    # set random seed for reproducibility
     ppsci.utils.misc.set_random_seed(42)
+
+    # set output directory
+    output_dir = "./output_0504_debug"
 
     # initialize logger
     ppsci.utils.logger.init_logger("ppsci", f"{output_dir}/train.log", "info")
     core.set_prim_eager_enabled(True)
 
+    # Hyper parameters
     EPOCHS = 500
-    L = 1
-    X_IN = 0
-    X_OUT = X_IN + L
-    P_OUT = 0  # pressure at the outlet of pipe
-    P_IN = 0.1  # pressure at the inlet of pipe
-    rInlet = 0.05
-    nPt = 100
-    unique_x = np.linspace(X_IN, X_OUT, nPt)
-    mu = 0.5 * (X_OUT - X_IN)
-    N_y = 20
-    x_2d = np.tile(unique_x, N_y)
-    x_2d = np.reshape(x_2d, (len(x_2d), 1))
-    nu = 1e-3
-    sigma = 0.1
-    scaleStart = -0.02
-    scaleEnd = 0
-    Ng = 50
-    scale_1d = np.linspace(scaleStart, scaleEnd, Ng, endpoint=True)
-    x, scale = ThreeD_mesh(x_2d, scale_1d)
-
-    # axisymetric boundary
-    R = (
-        scale
-        * 1
-        / math.sqrt(2 * np.pi * sigma**2)
-        * np.exp(-((x - mu) ** 2) / (2 * sigma**2))
-    )
-
-    # Generate stenosis
-    yUp = (rInlet - R) * np.ones_like(x)
-    yDown = (-rInlet + R) * np.ones_like(x)
-    idx = np.where(scale == scaleStart)
-    plt.figure()
-    plt.scatter(x[idx], yUp[idx])
-    plt.scatter(x[idx], yDown[idx])
-    plt.axis("equal")
-    plt.show()
-    plt.savefig("idealized_stenotid_vessel", bbox_inches="tight")
-
-    y = np.zeros([len(x), 1])
-    for x0 in unique_x:
-        index = np.where(x[:, 0] == x0)[0]
-        Rsec = max(yUp[index])
-        tmpy = np.linspace(-Rsec, Rsec, len(index)).reshape(len(index), -1)
-        y[index] = tmpy
-
-    RHO = 1
     BATCH_SIZE = 50
     LEARNING_RATE = 1e-3
     LAYER_NUMBER = 4 - 1
     HIDDEN_SIZE = 20
+
+    # Physic properties
+    P_OUT = 0  # pressure at the outlet of pipe
+    P_IN = 0.1  # pressure at the inlet of pipe
+    NU = 1e-3
+    RHO = 1
+
+    # Geometry
+    L = 1
+    X_IN = 0
+    X_OUT = X_IN + L
+    R_INLET = 0.05
+    unique_x = np.linspace(X_IN, X_OUT, 100)
+    mu = 0.5 * (X_OUT - X_IN)
+    N_y = 20
+    x_2d = np.tile(unique_x, N_y)
+    x_2d = np.reshape(x_2d, (len(x_2d), 1))
+    SIGMA = 0.1
+    SCALE_START = -0.02
+    SCALE_END = 0
+    scale_1d = np.linspace(SCALE_START, SCALE_END, 50, endpoint=True)
+    x, scale = ThreeD_mesh(x_2d, scale_1d)
+
+    # Axisymetric boundary
+    R = (
+        scale
+        * 1
+        / math.sqrt(2 * np.pi * SIGMA**2)
+        * np.exp(-((x - mu) ** 2) / (2 * SIGMA**2))
+    )
+
+    # Generate stenosis
+    y_up = (R_INLET - R) * np.ones_like(x)
+    y_down = (-R_INLET + R) * np.ones_like(x)
+    idx = np.where(scale == SCALE_START)
+    plt.figure()
+    plt.scatter(x[idx], y_up[idx])
+    plt.scatter(x[idx], y_down[idx])
+    plt.axis("equal")
+    plt.show()
+    plt.savefig("./plot/idealized_stenotid_vessel", bbox_inches="tight")
+
+    # Points and shuffle(for alignment)
+    y = np.zeros([len(x), 1])
+    for x0 in unique_x:
+        index = np.where(x[:, 0] == x0)[0]
+        Rsec = max(y_up[index])
+        tmpy = np.linspace(-Rsec, Rsec, len(index)).reshape(len(index), -1)
+        y[index] = tmpy
 
     index = [i for i in range(x.shape[0])]
     res = list(zip(x, y, scale))
@@ -239,10 +265,10 @@ if __name__ == "__main__":
                 R = (
                     scale
                     * 1
-                    / np.sqrt(2 * np.pi * sigma**2)
-                    * paddle.exp(-((x - mu) ** 2) / (2 * sigma**2))
+                    / np.sqrt(2 * np.pi * SIGMA**2)
+                    * paddle.exp(-((x - mu) ** 2) / (2 * SIGMA**2))
                 )
-                self.h = rInlet - R
+                self.h = R_INLET - R
                 u = out["u"]
                 # The no-slip condition of velocity on the wall
                 new_out["u"] = u * (self.h**2 - y**2)
@@ -270,7 +296,7 @@ if __name__ == "__main__":
     model_4.register_output_transform(shared_transform)
     model = ppsci.arch.ModelList([model_2, model_3, model_4])
 
-    optimizer2 = ppsci.optimizer.Adam(
+    optimizer = ppsci.optimizer.Adam(
         LEARNING_RATE, beta1=0.9, beta2=0.99, epsilon=10**-15
     )([model])
 
@@ -302,53 +328,27 @@ if __name__ == "__main__":
         model,
         {pde_constraint.name: pde_constraint},
         output_dir,
-        optimizer2,
+        optimizer,
         epochs=EPOCHS,
         iters_per_epoch=int(x.shape[0] / BATCH_SIZE),
         eval_during_train=False,
         save_freq=10,
         log_freq=1,
         equation=equation,
-        checkpoint_path="/workspace/wangguan/PaddleScience_Surrogate/examples/aneurysm/output_0504/checkpoints/epoch_280",
+        checkpoint_path="/workspace/wangguan/PaddleScience_Surrogate/examples/aneurysm/output_0504/checkpoints/epoch_500",
     )
 
     # solver.train()
-    def predict(
-        input_dict,
-        solver,
-    ):
-        for key, val in input_dict.items():
-            input_dict[key] = paddle.to_tensor(val, dtype="float32")
-        evaluator = ppsci.utils.expression.ExpressionSolver(
-            input_dict.keys(), ["u", "v", "p"], solver.model
-        )
-        output_expr_dict = {
-            "u": lambda d: d["u"],
-            "v": lambda d: d["v"],
-            "p": lambda d: d["p"],
-        }
-        for output_key, output_expr in output_expr_dict.items():
-            evaluator.add_target_expr(output_expr, output_key)
-        output_dict = evaluator(input_dict)
-        return output_dict
 
-    def single_test(x, y, scale, caseIdx):
+    def single_test(x, y, scale, solver):
         xt = paddle.to_tensor(x, dtype="float32")
         yt = paddle.to_tensor(y, dtype="float32")
         scalet = scale * paddle.ones_like(xt)
         net_in = {"x": xt, "y": yt, "scale": scalet}
         output_dict = predict(net_in, solver)
-        np.savez(
-            "./" + str(int(caseIdx)) + "ML_WallStress_uvp",
-            x_center=x,
-            y_center=y,
-            u_center=output_dict["u"],
-            v_center=output_dict["v"],
-            p_center=output_dict["p"],
-        )
         return output_dict
 
-    scale_test = np.load("aneurysm_scale0005to002_eval0to002mean001_3sigma.npz")[
+    scale_test = np.load("./data/aneurysm_scale0005to002_eval0to002mean001_3sigma.npz")[
         "scale"
     ]
     os.chdir("/workspace/wangguan/PaddleScience_Surrogate/examples/aneurysm")
@@ -370,13 +370,13 @@ if __name__ == "__main__":
         U = Data_NN["U"]
         n = len(x)
         output_dict = single_test(
-            x.reshape(n, 1), y.reshape(n, 1), np.ones((n, 1)) * scale, caseIdx
+            x.reshape(n, 1), y.reshape(n, 1), np.ones((n, 1)) * scale, solver
         )
         u, v, p = output_dict["u"], output_dict["v"], output_dict["p"]
         w = np.zeros_like(u)
         U = np.concatenate([u, v, w], axis=1)
 
-        # velocity U
+        # Streamwise velocity component u
         plt.figure()
         plt.subplot(212)
         plt.scatter(x, y, c=U[:, 0], vmin=min(U_CFD[:, 0]), vmax=max(U_CFD[:, 0]))
@@ -393,7 +393,7 @@ if __name__ == "__main__":
             bbox_inches="tight",
         )
 
-        # velocity V
+        # Spanwise velocity component v
         plt.figure()
         plt.subplot(212)
         plt.scatter(x, y, c=U[:, 1], vmin=min(U_CFD[:, 1]), vmax=max(U_CFD[:, 1]))
@@ -411,7 +411,7 @@ if __name__ == "__main__":
         )
         plt.close("all")
 
-        # wall shear stress
+        # Centerline wall shear profile tau_c
         Data_CFD_wss = np.load(path + str(caseIdx) + "CFD_wss.npz")
         unique_x = Data_CFD_wss["x"]
         wall_shear_mag_up = Data_CFD_wss["wss"]
@@ -442,7 +442,7 @@ if __name__ == "__main__":
         plt.ylabel(r"$\tau_{c}$", fontsize=16)
         plt.legend(prop={"size": 16})
         plt.savefig(
-            "plot/" + str(int(caseIdx)) + "_nu_" + str(nu) + "_wallshear_test.png",
+            "plot/" + str(int(caseIdx)) + "_nu_" + str(NU) + "_wallshear_test.png",
             bbox_inches="tight",
         )
         plt.close("all")
