4d933f39d32680e6383b1e9b56f0090a8d3b2608
wangguan <772359200@qq.com>
2023-06-06 06:39:52 +0000

    add reference

--- examples/aneurysm/aneurysm_flow.py
+++ examples/aneurysm/aneurysm_flow.py
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+"""
+Reference: https://github.com/Jianxun-Wang/LabelFree-DNN-Surrogate
+"""
+
 import math
 import os
 import os.path as osp
--- examples/pipe/poiseuille_flow.py
+++ examples/pipe/poiseuille_flow.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Reference: https://github.com/Jianxun-Wang/LabelFree-DNN-Surrogate
+"""
+
 import copy
 import os
 import os.path as osp
