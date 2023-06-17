6c689e025c659fd06890e80520021b21eace896c
jiamingkong <kinetical@live.com>
2023-06-17 14:28:16 +0800

    Black code

--- ppsci/test/equation/test_pde_base.py
+++ ppsci/test/equation/test_pde_base.py
@@ -1,19 +1,11 @@
 import pytest
-
 from ppsci.equation import PDE
 from paddle import nn
 import paddle
 import sympy
 
 
-def jacobian(y: "paddle.Tensor", x: "paddle.Tensor") -> "paddle.Tensor":
-    return paddle.grad(y, x, create_graph=True)[0]
-
-def hessian(y: "paddle.Tensor", x: "paddle.Tensor") -> "paddle.Tensor":
-    return jacobian(jacobian(y, x), x)
-
-
-class Test_PDE():
+class Test_PDE:
     def test_pde_init(self):
         """
         Testing the PDE class initialization
@@ -28,10 +20,11 @@ class Test_PDE():
         initiate a PDE object and add an equation to it
         """
         pde = PDE()
+
         def simple_equation(out):
             x, y = out["x"], out["y"]
             return x + y
-        
+
         pde.add_equation("simple", simple_equation)
 
         assert "simple" in pde.equations
@@ -43,7 +36,7 @@ class Test_PDE():
         def simple_equation2(out):
             x, y = out["x"], out["y"]
             return x - y
-        
+
         pde.add_equation("simple", simple_equation2)
 
         assert pde.equations["simple"] == simple_equation2
@@ -57,7 +50,7 @@ class Test_PDE():
         pde = PDE()
 
         # create symbols
-        x,y,z = pde.create_symbols("x y z")
+        x, y, z = pde.create_symbols("x y z")
         assert isinstance(x, sympy.Symbol)
         assert isinstance(y, sympy.Symbol)
         assert isinstance(z, sympy.Symbol)
@@ -69,7 +62,7 @@ class Test_PDE():
         pde = PDE()
 
         # create symbols
-        x,y,z = pde.create_symbols("x y z")
+        x, y, z = pde.create_symbols("x y z")
 
         # create a function
         f = pde.create_function(name="f", invars=(x, y, z))
@@ -86,7 +79,9 @@ class Test_PDE():
         assert len(pde.parameters()) == 0
 
         # add a learnable parameter
-        pde.learnable_parameters.append(paddle.create_parameter(shape=[1], dtype="float32"))
+        pde.learnable_parameters.append(
+            paddle.create_parameter(shape=[1], dtype="float32")
+        )
 
         # check parameters
         assert len(pde.parameters()) == 1
@@ -101,7 +96,9 @@ class Test_PDE():
         assert len(pde.state_dict()) == 0
 
         # add a learnable parameter
-        pde.learnable_parameters.append(paddle.create_parameter(shape=[1], dtype="float32"))
+        pde.learnable_parameters.append(
+            paddle.create_parameter(shape=[1], dtype="float32")
+        )
 
         # check parameters
         assert len(pde.state_dict()) == 1
@@ -116,13 +113,17 @@ class Test_PDE():
         assert len(pde.state_dict()) == 0
 
         # add a learnable parameter
-        pde.learnable_parameters.append(paddle.create_parameter(shape=[1], dtype="float32"))
+        pde.learnable_parameters.append(
+            paddle.create_parameter(shape=[1], dtype="float32")
+        )
 
         # check parameters
         assert len(pde.state_dict()) == 1
 
         external_state = {
-            "learnable_parameters": [paddle.create_parameter(shape=[1], dtype="float32")]
+            "learnable_parameters": [
+                paddle.create_parameter(shape=[1], dtype="float32")
+            ]
         }
 
         # set state dict
