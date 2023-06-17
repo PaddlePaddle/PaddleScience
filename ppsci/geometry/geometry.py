a9d8510068aab7ab83ad5d9b458ade5f67ccff9d
Liyulingyue <852433440@qq.com>
2023-06-17 13:16:50 +0800

    add if/else to pass ci

--- ppsci/geometry/geometry.py
+++ ppsci/geometry/geometry.py
@@ -95,10 +95,14 @@ class Geometry:
                     "please check correctness of geometry and given creteria."
                 )
 
-        sdf = -self.sdf_func(x)
-        sdf_dict = misc.convert_to_dict(sdf, ("sdf",))
-        x_dict = misc.convert_to_dict(x, self.dim_keys)
-        return {**x_dict, **sdf_dict}
+        # if sdf_func added, return x_dict and sdf_dict, else, only return the x_dict
+        if "sdf_func" in dir(self):
+            sdf = -self.sdf_func(x)
+            sdf_dict = misc.convert_to_dict(sdf, ("sdf",))
+            x_dict = misc.convert_to_dict(x, self.dim_keys)
+            return {**x_dict, **sdf_dict}
+        else:
+            return misc.convert_to_dict(x, self.dim_keys)
 
     def sample_boundary(self, n, random="pseudo", criteria=None, evenly=False):
         """Compute the random points in the geometry and return those meet criteria."""
