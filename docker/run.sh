58eb27f5cfc529aab69614e6bcdfbb02b3c607b6
sanbuphy <physicoada@gmail.com>
2023-06-19 15:10:41 +0800

    add docker

---
+++ docker/run.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/PaddlePaddle/PaddleScience.git
+
+cd PaddleScience && git checkout -b develop 6839369 && \
+export PYTHONPATH=$PWD:$PYTHONPATH && \
+cd examples/bracket && \
+wget https://paddle-org.bj.bcebos.com/paddlescience/datasets/bracket/bracket_data.tar && \
+tar -xvf bracket_data.tar 
+
+cd ..
+docker build . -t paddlescience:latest
+docker run --gpus all -it -v ./:/work paddlescience
