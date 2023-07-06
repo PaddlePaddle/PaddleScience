b9d9b1cf8482b38b64b4e2eb8b24c41d2419bb8c
HydrogenSulfate <490868991@qq.com>
2023-07-06 10:00:09 +0000

    refine code and docs for docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,14 +1,9 @@
-cd examples/bracket
-wget https://paddle-org.bj.bcebos.com/paddlescience/datasets/bracket/bracket_data.tar
-tar -xvf bracket_data.tar
-
-cd ..
 docker build . -t paddlescience:latest
 
 if [ -x "$(command -v nvidia-docker)" ]; then
     nvidia-docker run -it -v $PWD:/work paddlescience
 elif [ -x "$(command -v docker)" ]; then
-    docker run --gpus all -it -v $PWD/:/work paddlescience
+    docker run --gpus all -it -v $PWD:/work paddlescience
 else
-    echo "docker 和 nvidia-docker 都未安装,请先安装！"
+    echo "Please install docker or nvidia-docker first."
 fi
