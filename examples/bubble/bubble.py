b94ee598e7c9bf82a51af28c51f5c664bcfdc11b
liaoxin2 <liaoxinzhilu@gmail.com>
2023-08-14 11:58:16 +0000

    add some modifications

--- examples/bubble/bubble.py
+++ examples/bubble/bubble.py
@@ -27,25 +27,89 @@ if __name__ == "__main__":
     # set random seed for reproducibility
     ppsci.utils.misc.set_random_seed(42)
     # set output directory
-    OUTPUT_DIR = "./output_bubble_pinns_p" if not args.output_dir else args.output_dir
+    OUTPUT_DIR = "./output_bubble" if not args.output_dir else args.output_dir
     # initialize logger
     logger.init_logger("ppsci", f"{OUTPUT_DIR}/train.log", "info")
 
+    # load Data
+    data = scipy.io.loadmat("bubble.mat")
+    # Normalize data
+    p_max = data["p"].max(axis=0)
+    p_min = data["p"].min(axis=0)
+    p_norm = (data["p"] - p_min) / (p_max - p_min)
+    u_max = data["u"].max(axis=0)
+    u_min = data["u"].min(axis=0)
+    u_norm = (data["u"] - u_min) / (u_max - u_min)
+    v_max = data["v"].max(axis=0)
+    v_min = data["v"].min(axis=0)
+    v_norm = (data["v"] - v_min) / (v_max - v_min)
+
+    u_star = u_norm  # N x T
+    v_star = v_norm  # N x T
+    P_star = p_norm  # N x T
+    Phil_star = data["phil"]  # N x T
+    t_star = data["t"]  # T x 1
+    X_star = data["X"]  # N x 2
+
+    N = X_star.shape[0]
+    T = t_star.shape[0]
+
+    # Rearrange Data
+    XX = np.tile(X_star[:, 0:1], (1, T))  # N x T
+    YY = np.tile(X_star[:, 1:2], (1, T))  # N x T
+    TT = np.tile(t_star, (1, N)).T  # N x T
+
+    UU = u_star  # U_star[:,0,:] # N x T
+    VV = v_star  # U_star[:,1,:] # N x T
+    PP = P_star  # N x T
+    Phil = Phil_star  # N x T
+
+    x = XX.flatten()[:, None]  # NT x 1
+    y = YY.flatten()[:, None]  # NT x 1
+    t = TT.flatten()[:, None]  # NT x 1
+
+    u = UU.flatten()[:, None]  # NT x 1
+    v = VV.flatten()[:, None]  # NT x 1
+    p = PP.flatten()[:, None]  # NT x 1
+    phil = Phil.flatten()[:, None]  # NT x 1
+
+    idx = np.random.choice(N * T, int(N * T * 0.75), replace=False)
+    # train data
+    train_data = dict()
+    train_data["x"] = x[idx, :]
+    train_data["y"] = y[idx, :]
+    train_data["t"] = t[idx, :]
+    train_data["u"] = u[idx, :]
+    train_data["v"] = v[idx, :]
+    train_data["p"] = p[idx, :]
+    train_data["phil"] = phil[idx, :]
+    scipy.io.savemat("bubble_train.mat", train_data)
+    # valid Data
+    test_data = dict()
+    test_data["x"] = x
+    test_data["y"] = y
+    test_data["t"] = t
+    test_data["u"] = u
+    test_data["v"] = v
+    test_data["p"] = p
+    test_data["phil"] = phil
+    scipy.io.savemat("bubble_valid.mat", test_data)
+
     DATASET_PATH = "bubble_train.mat"
-    DATASET_PATH_VALID = "bubble_test.mat"
+    DATASET_PATH_VALID = "bubble_valid.mat"
 
     # set model
-    model_psi = ppsci.arch.MLP(("x", "y", "t"), ("psi",), 9, 30, "tanh")
-    model_p = ppsci.arch.MLP(("x", "y", "t"), ("p",), 9, 30, "tanh")
-    model_phil = ppsci.arch.MLP(("x", "y", "t"), ("phil",), 9, 30, "tanh")
+    model_psi = ppsci.arch.MLP(("t", "x", "y"), ("psi",), 9, 30, "tanh")
+    model_p = ppsci.arch.MLP(("t", "x", "y"), ("p",), 9, 30, "tanh")
+    model_phil = ppsci.arch.MLP(("t", "x", "y"), ("phil",), 9, 30, "tanh")
 
     def transform_out(in_, out):
         psi_y = out["psi"]
         y = in_["y"]
         x = in_["x"]
-        u_out = jacobian(psi_y, y)
-        v_out = -jacobian(psi_y, x)
-        return {"u": u_out, "v": v_out}
+        u = jacobian(psi_y, y)
+        v = -jacobian(psi_y, x)
+        return {"u": u, "v": v}
 
     model_psi.register_output_transform(transform_out)
     model_list = ppsci.arch.ModelList((model_psi, model_p, model_phil))
@@ -73,7 +137,7 @@ if __name__ == "__main__":
         "dataset": {
             "name": "MatDataset",
             "file_path": DATASET_PATH,
-            "input_keys": ("x", "t", "y"),
+            "input_keys": ("t", "x", "y"),
             "label_keys": ("u", "v", "p", "phil"),
             "timestamps": timestamps,
         },
@@ -151,16 +215,6 @@ if __name__ == "__main__":
         NPOINT_PDE * NTIME_PDE, evenly=True
     )
 
-    datafile = "maxmin.mat"
-    data = scipy.io.loadmat(datafile)
-    u_max = data["u_max"][0][0]
-    u_min = data["u_min"][0][0]
-    v_max = data["v_max"][0][0]
-    v_min = data["v_min"][0][0]
-    p_max = data["p_max"][0][0]
-    p_min = data["p_min"][0][0]
-    phil_max = data["phil_max"][0][0]
-    phil_min = data["phil_min"][0][0]
     visualizer = {
         "visulzie_u_v_p": ppsci.visualize.VisualizerVtu(
             visu_mat,
