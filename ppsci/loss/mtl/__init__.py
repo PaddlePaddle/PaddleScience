670db460f7cfc0ed81e27f0323cfe52cb00ca7b1
HydrogenSulfate <490868991@qq.com>
2023-08-18 15:03:39 +0800

    [Feature]add multi-task learning module and a method "PCGrad" (#492)
    * add PCGrad for multi-task learning
    
    * use LossAggregator for MTL module
    
    * refine code
    
    * refine backward code; remove type hint for build_mtl_aggregator; remove assert in PCGrad._set_grads
    
    * remove param count code to LossAggregator
    
    * remove losses from docstring of PCGrad

---
+++ ppsci/loss/mtl/__init__.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import copy
+
+from ppsci.loss.mtl.base import LossAggregator
+from ppsci.loss.mtl.pcgrad import PCGrad
+
+__all__ = [
+    "LossAggregator",
+    "PCGrad",
+]
+
+
+def build_mtl_aggregator(cfg):
+    """Build loss aggregator with multi-task learning method.
+
+    Args:
+        cfg (AttrDict): Aggregator config.
+    Returns:
+        Loss: Callable loss aggregator object.
+    """
+    cfg = copy.deepcopy(cfg)
+
+    aggregator_cls = cfg.pop("name")
+    aggregator = eval(aggregator_cls)(**cfg)
+    return aggregator
