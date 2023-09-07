50207e68a528cb0d54b49a978f28b0b831d7786f
yangguohao <1901212980@pku.edu.cn>
2023-09-07 20:00:06 +0800

    fix

--- ppsci/probability/__init__.py
+++ ppsci/probability/__init__.py
@@ -12,25 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import paddle
-import pytest
-
 from ppsci.probability.hmc import HamiltonianMonteCarlo
 
-paddle.seed(1024)
-
-
-@pytest.mark.parametrize("true_mean", [5.0])
-@pytest.mark.parametrize("true_std", [1.0])
-def test_HamiltonianMonteCarlo(true_mean, true_std):
-    dist = paddle.distribution.Normal(true_mean, true_std)
-    HMC = HamiltonianMonteCarlo(dist, path_len=1.5, step_size=0.25)
-    trial = HMC.run_chain(2500, paddle.to_tensor(0.0))
-
-    assert paddle.allclose(trial.mean(), paddle.to_tensor(true_mean), rtol=0.05)
-    assert paddle.allclose(paddle.std(trial), paddle.to_tensor(true_std), rtol=0.05)
-
-
-if __name__ == "__main__":
-    pytest.main()
+__all__ = [
+    "HamiltonianMonteCarlo",
+]
