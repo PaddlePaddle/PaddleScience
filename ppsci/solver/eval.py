031f2103a5a80db1807f706136b93f8396770e8f
HydrogenSulfate <490868991@qq.com>
2023-09-26 05:50:41 +0000

    fix bug in eval.py

--- ppsci/solver/eval.py
+++ ppsci/solver/eval.py
@@ -156,17 +156,17 @@ def _eval_by_dataset(
 
         # concate all data and discard padded sample(s)
         for key in all_input:
-            if paddle.is_tensor(all_input[key]):
+            if paddle.is_tensor(all_input[key][0]):
                 all_input[key] = paddle.concat(all_input[key])
             if len(all_input[key]) > num_samples:
                 all_input[key] = all_input[key][:num_samples]
         for key in all_output:
-            if paddle.is_tensor(all_input[key]):
+            if paddle.is_tensor(all_output[key][0]):
                 all_output[key] = paddle.concat(all_output[key])
             if len(all_output[key]) > num_samples:
                 all_output[key] = all_output[key][:num_samples]
         for key in all_label:
-            if paddle.is_tensor(all_input[key]):
+            if paddle.is_tensor(all_label[key][0]):
                 all_label[key] = paddle.concat(all_label[key])
             if len(all_label[key]) > num_samples:
                 all_label[key] = all_label[key][:num_samples]
