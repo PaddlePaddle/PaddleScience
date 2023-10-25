2f4976c1a453323d9667702e2a1b853b7cf3cae1
wangguan <772359200@qq.com>
2023-10-25 05:27:59 +0000

    fix err batch size != 1, when no sampler

--- ppsci/data/__init__.py
+++ ppsci/data/__init__.py
@@ -67,6 +67,7 @@ def build_dataloader(_dataset, cfg):
     cfg = copy.deepcopy(cfg)
 
     # build sampler
+    print("build sampler")
     sampler_cfg = cfg.pop("sampler", None)
     if sampler_cfg is not None:
         sampler_cls = sampler_cfg.pop("name")
@@ -81,6 +82,13 @@ def build_dataloader(_dataset, cfg):
         sampler_cfg["batch_size"] = cfg["batch_size"]
         sampler = getattr(io, sampler_cls)(_dataset, **sampler_cfg)
     else:
+        batch_size = cfg["batch_size"]
+        if batch_size != 1:
+            raise ValueError(f"batch_size : {batch_size} != 1")
+        logger.warning(
+                    f"No sampler specified, it means : input['x'] = NamedArrayDataset.__getitem__(i), batch_size = 1"
+                    f"More information can be found in [https://www.paddlepaddle.org.cn/documentation/docs/zh/api/paddle/io/DataLoader_cn.html]"
+                )
         sampler = None
 
     # build collate_fn if specified
