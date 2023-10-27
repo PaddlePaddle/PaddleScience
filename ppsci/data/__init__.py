38886c7cfb28e94459c5f31c590f95542206ceec
WG <39621324+wangguan1995@users.noreply.github.com>
2023-10-27 14:24:36 +0800

    Bugfix nosampler (#601)
    * fix err batch size != 1, when no sampler
    
    * delete print
    
    * Update ppsci/data/__init__.py
    
    * fix
    
    ---------
    
    Co-authored-by: HydrogenSulfate <490868991@qq.com>

--- ppsci/data/__init__.py
+++ ppsci/data/__init__.py
@@ -81,6 +81,13 @@ def build_dataloader(_dataset, cfg):
         sampler_cfg["batch_size"] = cfg["batch_size"]
         sampler = getattr(io, sampler_cls)(_dataset, **sampler_cfg)
     else:
+        if cfg["batch_size"] != 1:
+            raise ValueError(
+                f"`batch_size` should be 1 when sampler config is None, but got {cfg['batch_size']}."
+            )
+        logger.warning(
+            "`batch_size` is set to 1 as neither sampler config or batch_size is set."
+        )
         sampler = None
 
     # build collate_fn if specified
