2da3091445e5ef2b18dc69d683f8ade360d71d33
co63oc <co63oc@users.noreply.github.com>
2023-10-27 19:01:34 +0800

    【hydra No.12】deephpms (#603)
    * Add deephpms hydra
    
    * Fix
    
    * Fix

--- examples/deephpms/burgers.py
+++ examples/deephpms/burgers.py
@@ -12,13 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from os import path as osp
+
+import hydra
 import paddle
 import paddle.nn.functional as F
+from omegaconf import DictConfig
 
 import ppsci
 from ppsci.autodiff import hessian
 from ppsci.autodiff import jacobian
-from ppsci.utils import config
 from ppsci.utils import logger
 
 
@@ -49,26 +52,21 @@ def boundary_loss_func(output_dict, *args):
     return losses
 
 
-if __name__ == "__main__":
-    args = config.parse_args()
-    ppsci.utils.misc.set_random_seed(42)
-    DATASET_PATH = "./datasets/DeepHPMs/burgers_sine.mat"
-    DATASET_PATH_SOL = "./datasets/DeepHPMs/burgers.mat"
-    OUTPUT_DIR = "./output_burgers/" if args.output_dir is None else args.output_dir
-
+def train(cfg: DictConfig):
+    ppsci.utils.misc.set_random_seed(cfg.seed)
     # initialize logger
-    logger.init_logger("ppsci", f"{OUTPUT_DIR}/train.log", "info")
+    logger.init_logger("ppsci", osp.join(cfg.output_dir, f"{cfg.mode}.log"), "info")
 
     # initialize burgers boundaries
-    t_lb = paddle.to_tensor([0.0])
-    t_ub = paddle.to_tensor([10.0])
-    x_lb = paddle.to_tensor([-8.0])
-    x_ub = paddle.to_tensor([8.0])
+    t_lb = paddle.to_tensor(cfg.T_LB)
+    t_ub = paddle.to_tensor(cfg.T_UB)
+    x_lb = paddle.to_tensor(cfg.X_LB)
+    x_ub = paddle.to_tensor(cfg.T_UB)
 
     # initialize models
-    model_idn = ppsci.arch.MLP(("t", "x"), ("u_idn",), 4, 50, "sin")
-    model_pde = ppsci.arch.MLP(("u_x", "du_x", "du_xx"), ("f_pde",), 2, 100, "sin")
-    model_sol = ppsci.arch.MLP(("t", "x"), ("u_sol",), 4, 50, "sin")
+    model_idn = ppsci.arch.MLP(**cfg.MODEL.idn_net)
+    model_pde = ppsci.arch.MLP(**cfg.MODEL.pde_net)
+    model_sol = ppsci.arch.MLP(**cfg.MODEL.sol_net)
 
     # initialize transform
     def transform_u(_in):
@@ -101,28 +99,23 @@ if __name__ == "__main__":
     # initialize model list
     model_list = ppsci.arch.ModelList((model_idn, model_pde, model_sol))
 
-    # set training hyper-parameters
-    ITERS_PER_EPOCH = 1
-    EPOCHS = 50000 if args.epochs is None else args.epochs  # set 1 for LBFGS
-    # MAX_ITER = 50000  # for LBFGS
-
     # initialize optimizer
     # Adam
-    optimizer_idn = ppsci.optimizer.Adam(1e-3)(model_idn)
-    optimizer_pde = ppsci.optimizer.Adam(1e-3)(model_pde)
-    optimizer_sol = ppsci.optimizer.Adam(1e-3)(model_sol)
+    optimizer_idn = ppsci.optimizer.Adam(cfg.TRAIN.learning_rate)(model_idn)
+    optimizer_pde = ppsci.optimizer.Adam(cfg.TRAIN.learning_rate)(model_pde)
+    optimizer_sol = ppsci.optimizer.Adam(cfg.TRAIN.learning_rate)(model_sol)
 
     # LBFGS
-    # optimizer_idn = ppsci.optimizer.LBFGS(max_iter=MAX_ITER)((model_idn, ))
-    # optimizer_pde = ppsci.optimizer.LBFGS(max_iter=MAX_ITER)((model_pde, ))
-    # optimizer_sol = ppsci.optimizer.LBFGS(max_iter=MAX_ITER)((model_sol, ))
+    # optimizer_idn = ppsci.optimizer.LBFGS(max_iter=cfg.TRAIN.max_iter)((model_idn, ))
+    # optimizer_pde = ppsci.optimizer.LBFGS(max_iter=cfg.TRAIN.max_iter)((model_pde, ))
+    # optimizer_sol = ppsci.optimizer.LBFGS(max_iter=cfg.TRAIN.max_iter)((model_sol, ))
 
     # stage 1: training identification net
     # manually build constraint(s)
     train_dataloader_cfg_idn = {
         "dataset": {
             "name": "IterableMatDataset",
-            "file_path": DATASET_PATH,
+            "file_path": cfg.DATASET_PATH,
             "input_keys": ("t", "x"),
             "label_keys": ("u_idn",),
             "alias_dict": {"t": "t_train", "x": "x_train", "u_idn": "u_train"},
@@ -141,7 +134,7 @@ if __name__ == "__main__":
     eval_dataloader_cfg_idn = {
         "dataset": {
             "name": "IterableMatDataset",
-            "file_path": DATASET_PATH,
+            "file_path": cfg.DATASET_PATH,
             "input_keys": ("t", "x"),
             "label_keys": ("u_idn",),
             "alias_dict": {"t": "t_star", "x": "x_star", "u_idn": "u_star"},
@@ -161,12 +154,12 @@ if __name__ == "__main__":
     solver = ppsci.solver.Solver(
         model_list,
         constraint_idn,
-        OUTPUT_DIR,
+        cfg.output_dir,
         optimizer_idn,
         None,
-        EPOCHS,
-        ITERS_PER_EPOCH,
-        eval_during_train=False,
+        cfg.TRAIN.epochs,
+        cfg.TRAIN.iters_per_epoch,
+        eval_during_train=cfg.TRAIN.eval_during_train,
         validator=validator_idn,
     )
 
@@ -180,7 +173,7 @@ if __name__ == "__main__":
     train_dataloader_cfg_pde = {
         "dataset": {
             "name": "IterableMatDataset",
-            "file_path": DATASET_PATH,
+            "file_path": cfg.DATASET_PATH,
             "input_keys": ("t", "x"),
             "label_keys": ("du_t",),
             "alias_dict": {"t": "t_train", "x": "x_train", "du_t": "t_train"},
@@ -202,7 +195,7 @@ if __name__ == "__main__":
     eval_dataloader_cfg_pde = {
         "dataset": {
             "name": "IterableMatDataset",
-            "file_path": DATASET_PATH,
+            "file_path": cfg.DATASET_PATH,
             "input_keys": ("t", "x"),
             "label_keys": ("du_t",),
             "alias_dict": {"t": "t_star", "x": "x_star", "du_t": "t_star"},
@@ -225,12 +218,12 @@ if __name__ == "__main__":
     solver = ppsci.solver.Solver(
         model_list,
         constraint_pde,
-        OUTPUT_DIR,
+        cfg.output_dir,
         optimizer_pde,
         None,
-        EPOCHS,
-        ITERS_PER_EPOCH,
-        eval_during_train=False,
+        cfg.TRAIN.epochs,
+        cfg.TRAIN.iters_per_epoch,
+        eval_during_train=cfg.TRAIN.eval_during_train,
         validator=validator_pde,
     )
 
@@ -247,7 +240,7 @@ if __name__ == "__main__":
     train_dataloader_cfg_sol_f = {
         "dataset": {
             "name": "IterableMatDataset",
-            "file_path": DATASET_PATH_SOL,
+            "file_path": cfg.DATASET_PATH_SOL,
             "input_keys": ("t", "x"),
             "label_keys": ("du_t",),
             "alias_dict": {"t": "t_f_train", "x": "x_f_train", "du_t": "t_f_train"},
@@ -256,7 +249,7 @@ if __name__ == "__main__":
     train_dataloader_cfg_sol_init = {
         "dataset": {
             "name": "IterableMatDataset",
-            "file_path": DATASET_PATH_SOL,
+            "file_path": cfg.DATASET_PATH_SOL,
             "input_keys": ("t", "x"),
             "label_keys": ("u_sol",),
             "alias_dict": {"t": "t0", "x": "x0", "u_sol": "u0"},
@@ -265,7 +258,7 @@ if __name__ == "__main__":
     train_dataloader_cfg_sol_bc = {
         "dataset": {
             "name": "IterableMatDataset",
-            "file_path": DATASET_PATH_SOL,
+            "file_path": cfg.DATASET_PATH_SOL,
             "input_keys": ("t", "x"),
             "label_keys": ("x",),
             "alias_dict": {"t": "tb", "x": "xb"},
@@ -306,7 +299,7 @@ if __name__ == "__main__":
     eval_dataloader_cfg_sol = {
         "dataset": {
             "name": "IterableMatDataset",
-            "file_path": DATASET_PATH_SOL,
+            "file_path": cfg.DATASET_PATH_SOL,
             "input_keys": ("t", "x"),
             "label_keys": ("u_sol",),
             "alias_dict": {"t": "t_star", "x": "x_star", "u_sol": "u_star"},
@@ -328,12 +321,12 @@ if __name__ == "__main__":
     solver = ppsci.solver.Solver(
         model_list,
         constraint_sol,
-        OUTPUT_DIR,
+        cfg.output_dir,
         optimizer_sol,
         None,
-        EPOCHS,
-        ITERS_PER_EPOCH,
-        eval_during_train=False,
+        cfg.TRAIN.epochs,
+        cfg.TRAIN.iters_per_epoch,
+        eval_during_train=cfg.TRAIN.eval_during_train,
         validator=validator_sol,
     )
 
@@ -341,3 +334,21 @@ if __name__ == "__main__":
     solver.train()
     # evaluate after finished training
     solver.eval()
+
+
+def evaluate(cfg: DictConfig):
+    print("Not supported.")
+
+
+@hydra.main(version_base=None, config_path="./conf", config_name="burgers.yaml")
+def main(cfg: DictConfig):
+    if cfg.mode == "train":
+        train(cfg)
+    elif cfg.mode == "eval":
+        evaluate(cfg)
+    else:
+        raise ValueError(f"cfg.mode should in ['train', 'eval'], but got '{cfg.mode}'")
+
+
+if __name__ == "__main__":
+    main()
