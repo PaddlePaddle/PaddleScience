e6c4caa7ad370d9e4640ee8c3cb51f4347f83030
HydrogenSulfate <490868991@qq.com>
2023-11-01 11:17:18 +0000

    fix transform input arguments of tempoGAN

--- examples/tempoGAN/functions.py
+++ examples/tempoGAN/functions.py
@@ -445,14 +445,14 @@ class DataFuncs:
 
     def transform(
         self,
-        input_item: Dict[str, np.ndarray],
-        label_item: Dict[str, np.ndarray],
-        weight_item: Dict[str, np.ndarray],
+        *data: Dict[str, np.ndarray],
     ) -> Union[
         Dict[str, paddle.Tensor], Dict[str, paddle.Tensor], Dict[str, paddle.Tensor]
     ]:
         if self.tile_ratio == 1:
-            return input_item, label_item, weight_item
+            return data
+
+        input_item, label_item, weight_item, *args = data
         for _ in range(self.max_turn):
             rand_ratio = np.random.rand()
             density_low = self.cut_data(input_item["density_low"], rand_ratio)
@@ -462,7 +462,7 @@ class DataFuncs:
 
         input_item["density_low"] = density_low
         input_item["density_high"] = density_high
-        return input_item, label_item, weight_item
+        return input_item, label_item, weight_item, *args
 
     def cut_data(self, data: np.ndarray, rand_ratio: float) -> paddle.Tensor:
         # data: C,H,W
