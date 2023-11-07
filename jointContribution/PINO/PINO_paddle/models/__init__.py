dd153c63e9baec3a9d39ccf105ea04ec0c6bef24
linying zhang <lyzhang@buaa.edu.cn>
2023-11-07 16:09:35 +0100

    graphGalerkin

--- jointContribution/PINO/PINO_paddle/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .FCN import FCNet
-from .fourier2d import FNO2d
-from .fourier3d import FNO3d
--- jointContribution/PINO/PINO_paddle/train_utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .adam import Adam
-from .datasets import NSLoader, DarcyFlow
-from .losses import get_forcing, LpLoss
