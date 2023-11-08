9b455fba9ca7c4a2cb407a732b8c31e1cb5b5ecd
linying zhang <lyzhang@buaa.edu.cn>
2023-11-08 10:58:15 +0100

    update1

--- jointContribution/PINO/PINO_paddle/train_utils/eval_2d.py
+++ jointContribution/PINO/PINO_paddle/train_utils/eval_2d.py
@@ -1,7 +1,11 @@
 from tqdm import tqdm
 import numpy as np
+
 import paddle
+
 from .losses import LpLoss, darcy_loss, PINO_loss
+
+import matplotlib.pyplot as plt
 
 def eval_darcy(model,
                dataloader,
@@ -18,11 +22,28 @@ def eval_darcy(model,
     mollifier = paddle.sin(np.pi * mesh[..., 0]) * paddle.sin(np.pi * mesh[..., 1]) * 0.001
     f_val = []
     test_err = []
+    i = 0
+    fig, ax = plt.subplots(3,3)
     with paddle.no_grad():
         for x, y in pbar:
 
             pred = model(x).reshape(y.shape)
             pred = pred * mollifier
+            if i < 3:
+                ax[2][i].imshow(pred[0, :, :])
+                ax[2][i].set_title('prediction')
+                ax[1][i].imshow(y[0, :, :])
+                ax[1][i].set_title('ground truth')
+                ax[0][i].imshow(x[0, :, :, 0])
+                ax[0][i].set_title('input')
+
+                for k in range(3):
+                    ax[k][i].set_xlabel('x')
+                    ax[k][i].set_ylabel('y')
+            if i==3:
+                plt.tight_layout()
+                plt.savefig('result.png')
+            i+=1
             data_loss = myloss(pred, y)
             a = x[..., 0]
             f_loss = darcy_loss(pred, a)
@@ -58,10 +79,25 @@ def eval_burgers(model,
 
     test_err = []
     f_err = []
+    i = 0
+    fig, ax = plt.subplots(2,3)
     for x, y in pbar:
         x, y = x, y
         out = model(x).reshape(y.shape)
         data_loss = myloss(out, y)
+        if i<3:
+            ax[0][i].imshow(out[0, :, :])
+            ax[0][i].set_xlabel('x')
+            ax[0][i].set_ylabel('t')
+            ax[0][i].set_title('prediction')
+            ax[1][i].imshow(y[0, :, :])
+            ax[1][i].set_xlabel('x')
+            ax[1][i].set_ylabel('t')
+            ax[1][i].set_title('ground truth')
+        if i==3:
+            plt.tight_layout()
+            plt.savefig('result.png')
+        i+=1
         loss_u, f_loss = PINO_loss(out, x[:, 0, :, 0], v)
         test_err.append(data_loss.item())
         f_err.append(f_loss.item())
