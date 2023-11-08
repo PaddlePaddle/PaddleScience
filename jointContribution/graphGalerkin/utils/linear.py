2025754895fae7f9b78b12c123122973bdae5626
Linying Zhang <75937288+zlynna@users.noreply.github.com>
2023-11-08 11:13:10 +0100

    GCNGalerkin+PINO论文复现 (#566)
    * add submodule as a git submodule
    
    * pre commit
    
    * change the md file
    
    * md file
    
    * review
    
    * delete useless file
    
    * delete useless file
    
    * change the files according to the review
    
    * change graphGalerkin
    
    * change graphGalerkin
    
    * graphGalerkin
    
    * update1
    
    ---------
    
    Co-authored-by: lijialin03 <lijialin03@baidu.com>

---
+++ jointContribution/graphGalerkin/utils/linear.py
@@ -0,0 +1,158 @@
+from typing import Optional
+from collections import OrderedDict
+
+import copy
+import math
+
+import paddle
+from paddle import nn
+from paddle import Tensor
+import paddle.nn.functional as F
+from paddle.nn.initializer import Uniform
+
+import init as inits
+
+class Linear(nn.Layer):
+    r"""Applies a linear tranformation to the incoming data
+
+    .. math::
+        \mathbf{x}^{\prime} = \mathbf{x} \mathbf{W}^{\top} + \mathbf{b}
+
+    similar to :class:`torch.nn.Linear`.
+    It supports lazy initialization and customizable weight and bias
+    initialization.
+
+    Args:
+        in_channels (int): Size of each input sample. Will be initialized
+            lazily in case it is given as :obj:`-1`.
+        out_channels (int): Size of each output sample.
+        bias (bool, optional): If set to :obj:`False`, the layer will not learn
+            an additive bias. (default: :obj:`True`)
+        weight_initializer (str, optional): The initializer for the weight
+            matrix (:obj:`"glorot"`, :obj:`"uniform"`, :obj:`"kaiming_uniform"`
+            or :obj:`None`).
+            If set to :obj:`None`, will match default weight initialization of
+            :class:`torch.nn.Linear`. (default: :obj:`None`)
+        bias_initializer (str, optional): The initializer for the bias vector
+            (:obj:`"zeros"` or :obj:`None`).
+            If set to :obj:`None`, will match default bias initialization of
+            :class:`torch.nn.Linear`. (default: :obj:`None`)
+    """
+    def __init__(self, in_channels: int, out_channels: int, bias: bool = True,
+                 weight_initializer: Optional[str] = None,
+                 bias_initializer: Optional[str] = None):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.weight_initializer = weight_initializer
+        self.bias_initializer = bias_initializer
+        self._parameters = OrderedDict()
+
+        if in_channels > 0:
+            self.weight = paddle.create_parameter(shape=[in_channels, out_channels], dtype=paddle.float32)
+        else:
+            self.weight = nn.parameter.UninitializedParameter()
+            self._hook = self.register_forward_pre_hook(
+                self.initialize_parameters)
+
+        if bias:
+            self.bias = paddle.create_parameter(shape=[out_channels], dtype=paddle.float32)
+        else:
+            self.register_parameter('bias', None)
+
+        self._load_hook = self.register_state_dict_hook(
+            self._lazy_load_hook)
+
+        self.reset_parameters()
+
+    def __deepcopy__(self, memo):
+        out = Linear(self.in_channels, self.out_channels, self.bias
+                     is not None, self.weight_initializer,
+                     self.bias_initializer)
+        if self.in_channels > 0:
+            out.weight = copy.deepcopy(self.weight, memo)
+        if self.bias is not None:
+            out.bias = copy.deepcopy(self.bias, memo)
+        return out
+
+    def reset_parameters(self):
+        # if isinstance(self.weight, nn.parameter.UninitializedParameter):
+        if self.in_channels<=0:
+            pass
+        elif self.weight_initializer == 'glorot':
+            inits.glorot(self.weight)
+        elif self.weight_initializer == 'uniform':
+            bound = 1.0 / math.sqrt(self.weight.size(-1))
+            self.weight = paddle.create_parameter(shape=self.weight.shape, dtype=paddle.float32, attr=paddle.ParamAttr(initializer=Uniform(-bound, bound)))
+        elif self.weight_initializer == 'kaiming_uniform':
+            inits.kaiming_uniform(self.weight, fan=self.in_channels,
+                                  a=math.sqrt(5))
+        elif self.weight_initializer is None:
+            inits.kaiming_uniform(self.weight, fan=self.in_channels,
+                                  a=math.sqrt(5))
+        else:
+            raise RuntimeError(f"Linear layer weight initializer "
+                               f"'{self.weight_initializer}' is not supported")
+
+        # if isinstance(self.weight, nn.parameter.UninitializedParameter):
+        if self.in_channels<=0:
+            pass
+        elif self.bias is None:
+            pass
+        elif self.bias_initializer == 'zeros':
+            inits.zeros(self.bias)
+        elif self.bias_initializer is None:
+            inits.uniform(self.in_channels, self.bias)
+        else:
+            raise RuntimeError(f"Linear layer bias initializer "
+                               f"'{self.bias_initializer}' is not supported")
+
+    def forward(self, x: Tensor) -> Tensor:
+        """"""
+        return F.linear(x, self.weight, self.bias)
+
+    @paddle.no_grad()
+    def initialize_parameters(self, module, input):
+        if isinstance(self.weight, nn.parameter.UninitializedParameter):
+            self.in_channels = input[0].size(-1)
+            self.weight.materialize((self.out_channels, self.in_channels))
+            self.reset_parameters()
+        self._hook.remove()
+        delattr(self, '_hook')
+
+    def _save_to_state_dict(self, destination, prefix, keep_vars):
+        if isinstance(self.weight, nn.parameter.UninitializedParameter):
+            destination[prefix + 'weight'] = self.weight
+        else:
+            destination[prefix + 'weight'] = self.weight.detach()
+        if self.bias is not None:
+            destination[prefix + 'bias'] = self.bias.detach()
+
+    def _lazy_load_hook(self, state_dict, prefix, local_metadata, strict,
+                        missing_keys, unexpected_keys, error_msgs):
+
+        weight = state_dict[prefix + 'weight']
+        if isinstance(weight, nn.parameter.UninitializedParameter):
+            self.in_channels = -1
+            self.weight = nn.parameter.UninitializedParameter()
+            if not hasattr(self, '_hook'):
+                self._hook = self.register_forward_pre_hook(
+                    self.initialize_parameters)
+
+        elif isinstance(self.weight, nn.parameter.UninitializedParameter):
+            self.in_channels = weight.size(-1)
+            self.weight.materialize((self.out_channels, self.in_channels))
+            if hasattr(self, '_hook'):
+                self._hook.remove()
+                delattr(self, '_hook')
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.in_channels}, '
+                f'{self.out_channels}, bias={self.bias is not None})')
+    
+    def register_parameter(self, name: str, param) -> None:
+        if param is None:
+            self._parameters[name] = None
+        else:
+            self._parameters[name] = param
+
