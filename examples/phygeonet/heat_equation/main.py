875dbbae548c859f09449b710ed806f14222f62d
DUCH714 <807966083@qq.com>
2023-12-18 22:30:30 +0300

    add

---
+++ examples/phygeonet/heat_equation/main.py
@@ -0,0 +1,297 @@
+import hydra
+import numpy as np
+import paddle
+from omegaconf import DictConfig
+
+import ppsci
+
+
+def dfdx(f, dydeta, dydxi, Jinv, h=0.01):
+    dfdxi_internal = (
+        (
+            -f[:, :, :, 4:]
+            + 8 * f[:, :, :, 3:-1]
+            - 8 * f[:, :, :, 1:-3]
+            + f[:, :, :, 0:-4]
+        )
+        / 12
+        / h
+    )
+    dfdxi_left = (
+        (
+            -11 * f[:, :, :, 0:-3]
+            + 18 * f[:, :, :, 1:-2]
+            - 9 * f[:, :, :, 2:-1]
+            + 2 * f[:, :, :, 3:]
+        )
+        / 6
+        / h
+    )
+    dfdxi_right = (
+        (
+            11 * f[:, :, :, 3:]
+            - 18 * f[:, :, :, 2:-1]
+            + 9 * f[:, :, :, 1:-2]
+            - 2 * f[:, :, :, 0:-3]
+        )
+        / 6
+        / h
+    )
+    dfdxi = paddle.concat(
+        (dfdxi_left[:, :, :, 0:2], dfdxi_internal, dfdxi_right[:, :, :, -2:]), 3
+    )
+    dfdeta_internal = (
+        (
+            -f[:, :, 4:, :]
+            + 8 * f[:, :, 3:-1, :]
+            - 8 * f[:, :, 1:-3, :]
+            + f[:, :, 0:-4, :]
+        )
+        / 12
+        / h
+    )
+    dfdeta_low = (
+        (
+            -11 * f[:, :, 0:-3, :]
+            + 18 * f[:, :, 1:-2, :]
+            - 9 * f[:, :, 2:-1, :]
+            + 2 * f[:, :, 3:, :]
+        )
+        / 6
+        / h
+    )
+    dfdeta_up = (
+        (
+            11 * f[:, :, 3:, :]
+            - 18 * f[:, :, 2:-1, :]
+            + 9 * f[:, :, 1:-2, :]
+            - 2 * f[:, :, 0:-3, :]
+        )
+        / 6
+        / h
+    )
+    dfdeta = paddle.concat(
+        (dfdeta_low[:, :, 0:2, :], dfdeta_internal, dfdeta_up[:, :, -2:, :]), 2
+    )
+    dfdx = Jinv * (dfdxi * dydeta - dfdeta * dydxi)
+    return dfdx
+
+
+def dfdy(f, dxdxi, dxdeta, Jinv, h=0.01):
+    dfdxi_internal = (
+        (
+            -f[:, :, :, 4:]
+            + 8 * f[:, :, :, 3:-1]
+            - 8 * f[:, :, :, 1:-3]
+            + f[:, :, :, 0:-4]
+        )
+        / 12
+        / h
+    )
+    dfdxi_left = (
+        (
+            -11 * f[:, :, :, 0:-3]
+            + 18 * f[:, :, :, 1:-2]
+            - 9 * f[:, :, :, 2:-1]
+            + 2 * f[:, :, :, 3:]
+        )
+        / 6
+        / h
+    )
+    dfdxi_right = (
+        (
+            11 * f[:, :, :, 3:]
+            - 18 * f[:, :, :, 2:-1]
+            + 9 * f[:, :, :, 1:-2]
+            - 2 * f[:, :, :, 0:-3]
+        )
+        / 6
+        / h
+    )
+    dfdxi = paddle.concat(
+        (dfdxi_left[:, :, :, 0:2], dfdxi_internal, dfdxi_right[:, :, :, -2:]), 3
+    )
+
+    dfdeta_internal = (
+        (
+            -f[:, :, 4:, :]
+            + 8 * f[:, :, 3:-1, :]
+            - 8 * f[:, :, 1:-3, :]
+            + f[:, :, 0:-4, :]
+        )
+        / 12
+        / h
+    )
+    dfdeta_low = (
+        (
+            -11 * f[:, :, 0:-3, :]
+            + 18 * f[:, :, 1:-2, :]
+            - 9 * f[:, :, 2:-1, :]
+            + 2 * f[:, :, 3:, :]
+        )
+        / 6
+        / h
+    )
+    dfdeta_up = (
+        (
+            11 * f[:, :, 3:, :]
+            - 18 * f[:, :, 2:-1, :]
+            + 9 * f[:, :, 1:-2, :]
+            - 2 * f[:, :, 0:-3, :]
+        )
+        / 6
+        / h
+    )
+    dfdeta = paddle.concat(
+        (dfdeta_low[:, :, 0:2, :], dfdeta_internal, dfdeta_up[:, :, -2:, :]), 2
+    )
+    dfdy = Jinv * (dfdeta * dxdxi - dfdxi * dxdeta)
+    return dfdy
+
+
+def tikzplotlib_fix_ncols(obj):
+    """
+    workaround for matplotlib 3.6 renamed legend's _ncol to _ncols, which breaks tikzplotlib
+    """
+    if hasattr(obj, "_ncols"):
+        obj._ncol = obj._ncols
+    for child in obj.get_children():
+        tikzplotlib_fix_ncols(child)
+
+
+def train(cfg: DictConfig):
+    # initiallizer
+    SEED = cfg.seed
+    ppsci.utils.misc.set_random_seed(SEED)
+    data = np.load(cfg.date_dir)
+    coords = data["coords"]
+    jinvs = data["jinvs"]
+    dxdxis = data["dxdxis"]
+    dydxis = data["dydxis"]
+    dxdetas = data["dxdetas"]
+    dydetas = data["dydetas"]
+    nx = cfg.MODEL.nx
+    ny = cfg.MODEL.ny
+
+    NvarInput = cfg.MODEL.NvarInput
+    NvarOutput = cfg.MODEL.NvarOutput
+    padSingleSide = cfg.MODEL.padSingleSide
+    model = ppsci.arch.USCNN(
+        cfg.MODEL.input_keys,
+        cfg.MODEL.output_keys,
+        cfg.MODEL.h,
+        nx,
+        ny,
+        NvarInput,
+        NvarOutput,
+        padSingleSide,
+    )
+
+    optimizer = ppsci.optimizer.Adam(cfg.TRAIN.lr)(model)
+
+    sup_constraint_mres = ppsci.constraint.SupervisedConstraint(
+        {
+            "dataset": {
+                "name": "NamedArrayDataset",
+                "input": {
+                    "coords": coords,
+                    "jinvs": jinvs,
+                    "dxdxis": dxdxis,
+                    "dydxis": dydxis,
+                    "dxdetas": dxdetas,
+                    "dydetas": dydetas,
+                },
+            },
+            "batch_size": 1,
+            "iters_per_epoch": coords.shape[0],
+            "num_workers": 0,
+        },
+        ppsci.loss.FunctionalLoss(lambda out, label, weught: out["mRes"]),
+        name="mRes",
+    )
+
+    constraint_pde = {sup_constraint_mres.name: sup_constraint_mres}
+
+    def _transform_out(_input, _output, padSingleSide=padSingleSide):
+        outputV = _output["outputV"]
+        batchSize = outputV.shape[0]
+        Jinv = _input["jinvs"]
+        dxdxi = _input["dxdxis"]
+        dydxi = _input["dydxis"]
+        dxdeta = _input["dxdetas"]
+        dydeta = _input["dydetas"]
+        for j in range(batchSize):
+            outputV[j, 0, -padSingleSide:, padSingleSide:-padSingleSide] = 0
+            outputV[j, 0, :padSingleSide, padSingleSide:-padSingleSide] = 1
+            outputV[j, 0, padSingleSide:-padSingleSide, -padSingleSide:] = 1
+            outputV[j, 0, padSingleSide:-padSingleSide, 0:padSingleSide] = 1
+            outputV[j, 0, 0, 0] = 0.5 * (outputV[j, 0, 0, 1] + outputV[j, 0, 1, 0])
+            outputV[j, 0, 0, -1] = 0.5 * (outputV[j, 0, 0, -2] + outputV[j, 0, 1, -1])
+        dvdx = dfdx(outputV, dydeta, dydxi, Jinv)
+        d2vdx2 = dfdx(dvdx, dydeta, dydxi, Jinv)
+        dvdy = dfdy(outputV, dxdxi, dxdeta, Jinv)
+        d2vdy2 = dfdy(dvdy, dxdxi, dxdeta, Jinv)
+        continuity = d2vdy2 + d2vdx2
+        return {"mRes": paddle.mean(continuity**2)}
+
+    model.register_output_transform(_transform_out)
+    output_dir = cfg.output_dir
+    solver = ppsci.solver.Solver(
+        model,
+        constraint_pde,
+        output_dir,
+        optimizer,
+        epochs=cfg.TRAIN.epochs,
+        iters_per_epoch=coords.shape[0],
+        eval_with_no_grad=cfg.TRAIN.eval_with_no_grad,
+    )
+
+    solver.train()
+
+    solver.plot_loss_history()
+
+
+def evaluate(cfg: DictConfig):
+    SEED = cfg.seed
+    ppsci.utils.misc.set_random_seed(SEED)
+
+    data = np.load(cfg.date_dir)
+    coords = data["coords"]
+
+    OFV_sb = paddle.to_tensor(data["OFV_sb"])
+
+    ## create model
+    padSingleSide = 1
+    model = ppsci.arch.USCNN(**cfg.MODEL)
+    model.register_output_transform(None)
+    solver = ppsci.solver.Solver(
+        model,
+        pretrained_model_path=cfg.EVAL.pretrained_model_path,  ### the path of the model
+    )
+    outputV = solver.predict({"coords": paddle.to_tensor(coords)})
+    outputV = outputV["outputV"]
+    outputV[0, 0, -padSingleSide:, padSingleSide:-padSingleSide] = 0
+    outputV[0, 0, :padSingleSide, padSingleSide:-padSingleSide] = 1
+    outputV[0, 0, padSingleSide:-padSingleSide, -padSingleSide:] = 1
+    outputV[0, 0, padSingleSide:-padSingleSide, 0:padSingleSide] = 1
+    outputV[0, 0, 0, 0] = 0.5 * (outputV[0, 0, 0, 1] + outputV[0, 0, 1, 0])
+    outputV[0, 0, 0, -1] = 0.5 * (outputV[0, 0, 0, -2] + outputV[0, 0, 1, -1])
+    CNNVNumpy = outputV[0, 0, :, :]
+    ev = paddle.sqrt(
+        paddle.mean((OFV_sb - CNNVNumpy) ** 2) / paddle.mean(OFV_sb**2)
+    ).item()
+    print(ev)
+
+
+@hydra.main(version_base=None, config_path="./conf", config_name="conf.yaml")
+def main(cfg: DictConfig):
+    if cfg.mode == "train":
+        train(cfg)
+    elif cfg.mode == "eval":
+        evaluate(cfg)
+    else:
+        raise ValueError(f"cfg.mode should in ['train', 'eval'], but got '{cfg.mode}'")
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/phygeonet/heat_equation_parameterized_BC/main.py
@@ -0,0 +1,293 @@
+import hydra
+import numpy as np
+import paddle
+from omegaconf import DictConfig
+
+import ppsci
+
+
+def dfdx(f, dydeta, dydxi, Jinv, h=0.01):
+    dfdxi_internal = (
+        (
+            -f[:, :, :, 4:]
+            + 8 * f[:, :, :, 3:-1]
+            - 8 * f[:, :, :, 1:-3]
+            + f[:, :, :, 0:-4]
+        )
+        / 12
+        / h
+    )
+    dfdxi_left = (
+        (
+            -11 * f[:, :, :, 0:-3]
+            + 18 * f[:, :, :, 1:-2]
+            - 9 * f[:, :, :, 2:-1]
+            + 2 * f[:, :, :, 3:]
+        )
+        / 6
+        / h
+    )
+    dfdxi_right = (
+        (
+            11 * f[:, :, :, 3:]
+            - 18 * f[:, :, :, 2:-1]
+            + 9 * f[:, :, :, 1:-2]
+            - 2 * f[:, :, :, 0:-3]
+        )
+        / 6
+        / h
+    )
+    dfdxi = paddle.concat(
+        (dfdxi_left[:, :, :, 0:2], dfdxi_internal, dfdxi_right[:, :, :, -2:]), 3
+    )
+    dfdeta_internal = (
+        (
+            -f[:, :, 4:, :]
+            + 8 * f[:, :, 3:-1, :]
+            - 8 * f[:, :, 1:-3, :]
+            + f[:, :, 0:-4, :]
+        )
+        / 12
+        / h
+    )
+    dfdeta_low = (
+        (
+            -11 * f[:, :, 0:-3, :]
+            + 18 * f[:, :, 1:-2, :]
+            - 9 * f[:, :, 2:-1, :]
+            + 2 * f[:, :, 3:, :]
+        )
+        / 6
+        / h
+    )
+    dfdeta_up = (
+        (
+            11 * f[:, :, 3:, :]
+            - 18 * f[:, :, 2:-1, :]
+            + 9 * f[:, :, 1:-2, :]
+            - 2 * f[:, :, 0:-3, :]
+        )
+        / 6
+        / h
+    )
+    dfdeta = paddle.concat(
+        (dfdeta_low[:, :, 0:2, :], dfdeta_internal, dfdeta_up[:, :, -2:, :]), 2
+    )
+    dfdx = Jinv * (dfdxi * dydeta - dfdeta * dydxi)
+    return dfdx
+
+
+def dfdy(f, dxdxi, dxdeta, Jinv, h=0.01):
+    dfdxi_internal = (
+        (
+            -f[:, :, :, 4:]
+            + 8 * f[:, :, :, 3:-1]
+            - 8 * f[:, :, :, 1:-3]
+            + f[:, :, :, 0:-4]
+        )
+        / 12
+        / h
+    )
+    dfdxi_left = (
+        (
+            -11 * f[:, :, :, 0:-3]
+            + 18 * f[:, :, :, 1:-2]
+            - 9 * f[:, :, :, 2:-1]
+            + 2 * f[:, :, :, 3:]
+        )
+        / 6
+        / h
+    )
+    dfdxi_right = (
+        (
+            11 * f[:, :, :, 3:]
+            - 18 * f[:, :, :, 2:-1]
+            + 9 * f[:, :, :, 1:-2]
+            - 2 * f[:, :, :, 0:-3]
+        )
+        / 6
+        / h
+    )
+    dfdxi = paddle.concat(
+        (dfdxi_left[:, :, :, 0:2], dfdxi_internal, dfdxi_right[:, :, :, -2:]), 3
+    )
+    dfdeta_internal = (
+        (
+            -f[:, :, 4:, :]
+            + 8 * f[:, :, 3:-1, :]
+            - 8 * f[:, :, 1:-3, :]
+            + f[:, :, 0:-4, :]
+        )
+        / 12
+        / h
+    )
+    dfdeta_low = (
+        (
+            -11 * f[:, :, 0:-3, :]
+            + 18 * f[:, :, 1:-2, :]
+            - 9 * f[:, :, 2:-1, :]
+            + 2 * f[:, :, 3:, :]
+        )
+        / 6
+        / h
+    )
+    dfdeta_up = (
+        (
+            11 * f[:, :, 3:, :]
+            - 18 * f[:, :, 2:-1, :]
+            + 9 * f[:, :, 1:-2, :]
+            - 2 * f[:, :, 0:-3, :]
+        )
+        / 6
+        / h
+    )
+    dfdeta = paddle.concat(
+        (dfdeta_low[:, :, 0:2, :], dfdeta_internal, dfdeta_up[:, :, -2:, :]), 2
+    )
+    dfdy = Jinv * (dfdeta * dxdxi - dfdxi * dxdeta)
+    return dfdy
+
+
+def tikzplotlib_fix_ncols(obj):
+    """
+    workaround for matplotlib 3.6 renamed legend's _ncol to _ncols, which breaks tikzplotlib
+    """
+    if hasattr(obj, "_ncols"):
+        obj._ncol = obj._ncols
+    for child in obj.get_children():
+        tikzplotlib_fix_ncols(child)
+
+
+def train(cfg: DictConfig):
+    # initiallizer
+    SEED = cfg.seed
+    ppsci.utils.misc.set_random_seed(SEED)
+
+    padSingleSide = cfg.MODEL.padSingleSide
+
+    model = ppsci.arch.USCNN(**cfg.MODEL)
+
+    optimizer = ppsci.optimizer.Adam(cfg.TRAIN.lr)(model)
+
+    data = np.load(cfg.date_dir)
+    coords = data["coords"]
+    jinvs = data["jinvs"]
+    dxdxis = data["dxdxis"]
+    dydxis = data["dydxis"]
+    dxdetas = data["dxdetas"]
+    dydetas = data["dydetas"]
+    len_data = cfg.len_data
+
+    sup_constraint_mres = ppsci.constraint.SupervisedConstraint(
+        {
+            "dataset": {
+                "name": "NamedArrayDataset",
+                "input": {
+                    "coords": coords,
+                    "jinvs": jinvs,
+                    "dxdxis": dxdxis,
+                    "dydxis": dydxis,
+                    "dxdetas": dxdetas,
+                    "dydetas": dydetas,
+                },
+            },
+            "batch_size": 1,
+            "iters_per_epoch": coords.shape[0],
+            "num_workers": 0,
+        },
+        ppsci.loss.FunctionalLoss(lambda out, label, weught: out["mRes"]),
+        name="mRes",
+    )
+
+    constraint_pde = {sup_constraint_mres.name: sup_constraint_mres}
+
+    def _transform_out(_input, outputV, padSingleSide=padSingleSide, k=len_data):
+        outputV = outputV["outputV"]
+        batchSize = outputV.shape[0]
+        Jinv = _input["jinvs"]
+        dxdxi = _input["dxdxis"]
+        dydxi = _input["dydxis"]
+        dxdeta = _input["dxdetas"]
+        dydeta = _input["dydetas"]
+        Para = _input["coords"]
+        for j in range(batchSize):
+            # Impose BC
+            outputV[j, 0, -padSingleSide:, padSingleSide:-padSingleSide] = outputV[
+                j, 0, 1:2, padSingleSide:-padSingleSide
+            ]
+            outputV[j, 0, :padSingleSide, padSingleSide:-padSingleSide] = outputV[
+                j, 0, -2:-1, padSingleSide:-padSingleSide
+            ]
+            outputV[j, 0, :, -padSingleSide:] = 0
+            outputV[j, 0, :, 0:padSingleSide] = Para[j, 0, 0, 0]
+        dvdx = dfdx(outputV, dydeta, dydxi, Jinv)
+        d2vdx2 = dfdx(dvdx, dydeta, dydxi, Jinv)
+        dvdy = dfdy(outputV, dxdxi, dxdeta, Jinv)
+        d2vdy2 = dfdy(dvdy, dxdxi, dxdeta, Jinv)
+        continuity = d2vdy2 + d2vdx2
+        return {"mRes": paddle.mean(continuity**2) / k}
+
+    model.register_output_transform(_transform_out)
+    solver = ppsci.solver.Solver(
+        model,
+        constraint_pde,
+        cfg.output_dir,
+        optimizer,
+        epochs=cfg.TRAIN.epochs,
+        iters_per_epoch=coords.shape[0],
+        eval_with_no_grad=cfg.TRAIN.eval_with_no_grad,
+    )
+
+    solver.train()
+    solver.plot_loss_history()
+
+
+def evaluate(cfg: DictConfig):
+    SEED = cfg.seed
+    ppsci.utils.misc.set_random_seed(SEED)
+
+    padSingleSide = cfg.MODEL.padSingleSide
+
+    model = ppsci.arch.USCNN(**cfg.MODEL)
+
+    model.register_output_transform(None)
+    data = np.load(cfg.date_dir)
+    coords = paddle.to_tensor(data["coords"])
+    truth = paddle.to_tensor(data["truths"])
+    len_data = cfg.len_data
+    solver = ppsci.solver.Solver(
+        model,
+        pretrained_model_path=cfg.EVAL.pretrained_model_path,  ### the path of the model
+    )
+
+    outputV = solver.predict({"coords": coords})
+    outputV = outputV["outputV"]
+    batchSize = outputV.shape[0]
+    for j in range(batchSize):
+        # Impose BC
+        outputV[j, 0, -padSingleSide:, padSingleSide:-padSingleSide] = outputV[
+            j, 0, 1:2, padSingleSide:-padSingleSide
+        ]
+        outputV[j, 0, :padSingleSide, padSingleSide:-padSingleSide] = outputV[
+            j, 0, -2:-1, padSingleSide:-padSingleSide
+        ]
+        outputV[j, 0, :, -padSingleSide:] = 0
+        outputV[j, 0, :, 0:padSingleSide] = coords[j, 0, 0, 0]
+    eV = paddle.sqrt(
+        paddle.mean((truth - outputV) ** 2) / paddle.mean(truth**2)
+    ).item()
+    print(eV / len_data)
+
+
+@hydra.main(version_base=None, config_path="./conf", config_name="conf.yaml")
+def main(cfg: DictConfig):
+    if cfg.mode == "train":
+        train(cfg)
+    elif cfg.mode == "eval":
+        evaluate(cfg)
+    else:
+        raise ValueError(f"cfg.mode should in ['train', 'eval'], but got '{cfg.mode}'")
+
+
+if __name__ == "__main__":
+    main()
