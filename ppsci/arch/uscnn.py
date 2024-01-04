bcfd461d67d2e3332ba3cdd5f674dddd6a4e9c6e
HydrogenSulfate <490868991@qq.com>
2024-01-04 09:59:49 +0000

    format phygeonet doc and fix uscnn examples

--- ppsci/arch/uscnn.py
+++ ppsci/arch/uscnn.py
@@ -24,18 +24,20 @@ class USCNN(base.Arch):
         k (int, optional): kernel_size. Defaults to 5.
         s (int, optional): stride. Defaults to 1.
         p (int, optional): padding. Defaults to 2.
-        Examples:
-            >>> import ppsci
-            >>> model = ppsci.arch.USCNN(
-                  input_keys= [ 'coords' ]
-                  output_keys= [ 'outputV' ]
-                  h= 0.01
-                  ny= 19
-                  nx= 84
-                  nvar_inp= 2
-                  nvar_out= 1
-                  pad_singleside= 1
-                )
+
+    Examples:
+        >>> import ppsci
+        >>> model = ppsci.arch.USCNN(
+        ...     ["coords"],
+        ...     ["outputV"],
+        ...     [16, 32, 16],
+        ...     h=0.01,
+        ...     ny=19,
+        ...     nx=84,
+        ...     nvar_in=2,
+        ...     nvar_out=1,
+        ...     pad_singleside=1,
+        ... )
     """
 
     def __init__(
