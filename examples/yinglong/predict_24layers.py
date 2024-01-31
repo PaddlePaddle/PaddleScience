3df851b52a858b356dd6c6e807f4dbb0d84c4ad3
zzm <95690929+zhiminzhang0830@users.noreply.github.com>
2024-01-31 20:05:25 +0800

    Yinglong infer (#772)
    * add yinglong inference code
    
    * add readme file for yinglong
    
    * update yinglong predict code
    
    * add copyright in predict_12layers.py
    
    * update readme
    
    * add reference for timefeatures.py
    
    * update predict_12layers and readme
    
    * add visualize and update readme
    
    * add figure for yinglong
    
    * update yinglong readme
    
    * fix readme for yinglong
    
    * add predict for yinglong 24 layers
    
    * update readme
    
    * add plot for yinglong
    
    * update predict code
    
    * update readme
    
    * update file path
    
    * add version check
    
    * fix doc string
    
    * update readme

--- examples/yinglong/predict_24layers.py
+++ examples/yinglong/predict_24layers.py
@@ -31,31 +31,31 @@ def parse_args():
     parser.add_argument(
         "--model_file",
         type=str,
-        default="./yinglong_models/yinglong_12.pdmodel",
+        default="./yinglong_models/yinglong_24.pdmodel",
         help="Model filename",
     )
     parser.add_argument(
         "--params_file",
         type=str,
-        default="./yinglong_models/yinglong_12.pdiparams",
+        default="./yinglong_models/yinglong_24.pdiparams",
         help="Parameter filename",
     )
     parser.add_argument(
         "--mean_path",
         type=str,
-        default="./hrrr_example_24vars/stat/mean_crop.npy",
+        default="./hrrr_example_69vars/stat/mean_crop.npy",
         help="Mean filename",
     )
     parser.add_argument(
         "--std_path",
         type=str,
-        default="./hrrr_example_24vars/stat/std_crop.npy",
+        default="./hrrr_example_69vars/stat/std_crop.npy",
         help="Standard deviation filename",
     )
     parser.add_argument(
         "--input_file",
         type=str,
-        default="./hrrr_example_24vars/valid/2022/01/01.h5",
+        default="./hrrr_example_69vars/valid/2022/01/01.h5",
         help="Input filename",
     )
     parser.add_argument(
@@ -64,14 +64,14 @@ def parse_args():
     parser.add_argument(
         "--nwp_file",
         type=str,
-        default="./hrrr_example_24vars/nwp_convert/2022/01/01/00.h5",
+        default="./hrrr_example_69vars/nwp_convert/2022/01/01/00.h5",
         help="NWP filename",
     )
     parser.add_argument(
         "--num_timestamps", type=int, default=22, help="Number of timestamps"
     )
     parser.add_argument(
-        "--output_path", type=str, default="output", help="Output file path"
+        "--output_path", type=str, default="output_24layers", help="Output file path"
     )
 
     return parser.parse_args()
@@ -100,18 +100,23 @@ def main():
     )
 
     # load data
-    # HRRR Crop use 24 atmospheric variable，their index in the dataset is from 0 to 23.
-    # The variable name is 'z50', 'z500', 'z850', 'z1000', 't50', 't500', 't850', 'z1000',
-    # 's50', 's500', 's850', 's1000', 'u50', 'u500', 'u850', 'u1000', 'v50', 'v500',
-    # 'v850', 'v1000', 'mslp', 'u10', 'v10', 't2m'.
+    # HRRR Crop use 69 atmospheric variable，their index in the dataset is from 0 to 68.
+    # The variable name is "z50", "z100",  "z150", "z200", "z250", "z300", "z400", "z500",
+    # "z600", "z700",  "z850", "z925", "z1000", "t50", "t100", "t150", "t200", "t250",
+    # "t300", "t400", "t500", "t600", "t700", "t850", "t925", "t1000", "s50", "s100",
+    # "s150", "s200",  "s250", "s300", "s400", "s500", "s600", "s700", "s850", "s925",
+    # "s1000", "u50", "u100", "u150", "u200", "u250", "u300", "u400", "u500", "u600",
+    # "u700", "u850", "u925", "u1000", "v50", "v100", "v150", "v200", "v250", "v300",
+    # "v400", "v500", "v600", "v700", "v850", "v925", "v1000",  "mslp", "u10", "v10",
+    # "t2m",
     input_file = h5py.File(args.input_file, "r")["fields"]
     nwp_file = h5py.File(args.nwp_file, "r")["fields"]
 
-    # input_data.shape: (1, 24, 440, 408)
+    # input_data.shape: (1, 69, 440, 408)
     input_data = input_file[0:1]
-    # nwp_data.shape: # (num_timestamps, 24, 440, 408)
+    # nwp_data.shape: # (num_timestamps, 69, 440, 408)
     nwp_data = nwp_file[0:num_timestamps]
-    # ground_truth.shape: (num_timestamps, 24, 440, 408)
+    # ground_truth.shape: (num_timestamps, 69, 440, 408)
     ground_truth = input_file[1 : num_timestamps + 1]
 
     # create time stamps
@@ -123,7 +128,7 @@ def main():
 
     # run predictor
     pred_data = predictor(input_data, time_stamps, nwp_data)
-    pred_data = pred_data.squeeze(axis=1)  # (num_timestamps, 24, 440, 408)
+    pred_data = pred_data.squeeze(axis=1)  # (num_timestamps, 69, 440, 408)
 
     # save predict data
     save_path = osp.join(args.output_path, "result.npy")
@@ -131,7 +136,7 @@ def main():
     logger.info(f"Save output to {save_path}")
 
     # plot wind data
-    u10_idx, v10_idx = 21, 22
+    u10_idx, v10_idx = 66, 67
     pred_wind = (pred_data[:, u10_idx] ** 2 + pred_data[:, v10_idx] ** 2) ** 0.5
     ground_truth_wind = (
         ground_truth[:, u10_idx] ** 2 + ground_truth[:, v10_idx] ** 2
@@ -142,7 +147,7 @@ def main():
         visu_key = f"Init time: {args.init_time}h\n Ground truth: {i+1}h"
         visu_keys.append(visu_key)
         data_dict[visu_key] = ground_truth_wind[i]
-        visu_key = f"Init time: {args.init_time}h\n YingLong-12 Layers: {i+1}h"
+        visu_key = f"Init time: {args.init_time}h\n YingLong-24 Layers: {i+1}h"
         visu_keys.append(visu_key)
         data_dict[visu_key] = pred_wind[i]
 
