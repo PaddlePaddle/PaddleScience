5fa44f2732c111e5de27c395b3bf4376b09234d5
Tianchi <62602289+DUCH714@users.noreply.github.com>
2024-03-25 11:59:27 +0300

    Add files via upload

--- examples/causalpinn/CausalPINN.py
+++ examples/causalpinn/CausalPINN.py
@@ -45,8 +45,8 @@ class modified_MLP_II(paddle.nn.Layer):
         self.M_t = M_t
         self.U1 = paddle.nn.Linear(in_features=layers[0], out_features=layers[1])
         self.U2 = paddle.nn.Linear(in_features=layers[0], out_features=layers[1])
-        self.Ws = [paddle.nn.Linear(in_features=layers[i], out_features=layers[i + 1]) for i in
-                   range(0, len(layers) - 2)]
+        self.Ws = paddle.nn.LayerList([paddle.nn.Linear(in_features=layers[i], out_features=layers[i + 1]) for i in
+                   range(0, len(layers) - 2)])
         self.F = paddle.nn.Linear(in_features=layers[-2], out_features=layers[-1])
         self.activation = activation
         self.k_t = paddle.pow(paddle.to_tensor(10), paddle.arange(0, self.M_t + 1)).astype('float32')
@@ -73,7 +73,7 @@ class modified_MLP_II(paddle.nn.Layer):
         return outputs
 
 class PINN((paddle.nn.Layer)):
-    def __init__(self, w_exact, layers, M_t, M_x, M_y, state0, t0, t1, n_t, x_star, y_star,x_starall, y_starall,t_starall, tol):
+    def __init__(self, w_exact, layers, M_t, M_x, M_y, state0, t0, t1, n_t, x_star, y_star,t_star, tol):
         super(PINN, self).__init__()
         self.w_exact = w_exact
 
@@ -89,9 +89,7 @@ class PINN((paddle.nn.Layer)):
         self.t = paddle.linspace(self.t0, self.t1 + eps, n_t)
         self.x_star = x_star
         self.y_star = y_star
-        self.x_starall = x_starall
-        self.y_starall = y_starall
-        self.t_starall=t_starall
+        self.t_star=t_star
 
         # initial state
         self.state0 = state0
@@ -103,7 +101,10 @@ class PINN((paddle.nn.Layer)):
                                        activation=nn.Tanh())
 
         # Use optimizers to set optimizer initialization and update functions
-        self.optimizer = paddle.optimizer.Adam(learning_rate=paddle.optimizer.lr.ExponentialDecay(1e-3, gamma=0.9),
+        # self.optimizer = paddle.optimizer.Adam(learning_rate=paddle.optimizer.lr.ExponentialDecay(1e-3, gamma=0.9),
+        #                                        parameters=self.network.parameters())
+
+        self.optimizer = paddle.optimizer.Adam(learning_rate=1e-3,
                                                parameters=self.network.parameters())
 
         # Logger
@@ -142,7 +143,7 @@ class PINN((paddle.nn.Layer)):
 
     def residuals_and_weights(self, tol, batch):
         t_r, x_r,y_r = batch
-        loss_u0, loss_v0, loss_w0 = self.loss_ics()
+        loss_u0, loss_v0, loss_w0 = self.loss_ics(self.x_star,self.y_star,paddle.zeros_like(self.x_star,dtype='float32'))
         L_0 = 1e5 * (loss_u0 + loss_v0 + loss_w0)
         res_w_pred, res_c_pred = self.residual_net(t_r, x_r, y_r)
         L_t = paddle.mean(res_w_pred ** 2 + 100 * res_c_pred ** 2, axis=1)
@@ -150,13 +151,13 @@ class PINN((paddle.nn.Layer)):
         W.stop_gradient=True
         return L_0, L_t, W
 
-    def loss_ics(self):
+    def loss_ics(self,x,y,t):
         # Compute forward pass
-        u_v = self.network(paddle.concat([self.x_star, self.y_star, paddle.zeros_like(self.x_star,dtype='float32')], 1))
+        u_v = self.network(paddle.concat([x,y,t], 1))
         u0_pred = u_v[:, 0:1]
         v0_pred = u_v[:, 1:2]
-        v_x = paddle.grad(v0_pred, self.x_star, create_graph=False)[0]
-        u_y = paddle.grad(u0_pred, self.y_star, create_graph=False)[0]
+        v_x = paddle.grad(v0_pred, x, create_graph=False)[0]
+        u_y = paddle.grad(u0_pred, y, create_graph=False)[0]
         w0_pred = v_x - u_y
         # Compute loss
         loss_u0 = paddle.mean((u0_pred.flatten() - self.state0[0, :, :].flatten()) ** 2)
@@ -170,15 +171,15 @@ class PINN((paddle.nn.Layer)):
         loss = paddle.mean(W * (L_t.reshape((-1,32)).T) + L_0)
         return loss
 
-    def compute_l2_error(self):
-        u_v = self.network(paddle.concat([x_star, y_star,t_star[:num_step],], 1))
+    def compute_l2_error(self,x,y,t):
+        u_v = self.network(paddle.concat([x,y,t], 1))
         u = u_v[:, 0:1]
         v = u_v[:, 1:2]
 
-        u_y = paddle.grad(u, y_star, create_graph=False)[0]
-        v_x = paddle.grad(v, x_star, create_graph=False)[0]
+        u_y = paddle.grad(u, y, create_graph=False)[0]
+        v_x = paddle.grad(v, x, create_graph=False)[0]
         w_pred = v_x - u_y
-        l2_error = np.linalg.norm(w_pred - self.w_exact) / np.linalg.norm(self.w_exact)
+        l2_error = paddle.linalg.norm(w_pred - paddle.transpose(self.w_exact,(1,2,0)).reshape((-1,1))) / paddle.linalg.norm(self.w_exact.reshape((-1,1)))
         return l2_error
 
     # Optimize parameters in a loop
@@ -192,10 +193,10 @@ class PINN((paddle.nn.Layer)):
             self.optimizer.step()
             self.optimizer.clear_grad()
 
-            if it % 1000 == 0:
-                print("ite:{},loss:{:.3e}".format(it,loss.item()))
-                # l2_error_value = self.compute_l2_error()
-
+            if it % 1 == 0:
+                l2_error_value = self.compute_l2_error(paddle.tile(self.x_star, (1, num_step)).reshape((-1,1)),paddle.tile(self.y_star, (1, num_step)).reshape((-1,1)),(paddle.tile(self.t_star[:num_step], (1, Nx**2)).T).reshape((-1,1)))
+                print("ite:{},loss:{:.3e},error:{:.3e}".format(it,self.loss(batch).item(),l2_error_value.item()))
+                # print("ite:{},loss:{:.3e}".format(it,loss.item()))
                 _, _, W_value = self.residuals_and_weights(self.tol, batch)
 
                 # self.l2_error_log.append(l2_error_value)
@@ -203,8 +204,7 @@ class PINN((paddle.nn.Layer)):
                 if W_value.min() > 0.99:
                     break
 
-np.random.seed(1234)
-
+paddle.seed(1234)
 data = np.load('/home/aistudio/data/data262374/NS.npy', allow_pickle=True).item()
 # Test data
 sol = paddle.to_tensor(data['sol'])
@@ -219,9 +219,6 @@ sol = sol
 x_star, y_star=paddle.meshgrid(x_star,y_star)
 x_star=x_star.reshape((-1,1))
 y_star=y_star.reshape((-1,1))
-x_starall = paddle.tile(x_star, (1, Nt)).reshape((-1,1))  # N x T
-y_starall = paddle.tile(y_star, (1, Nt)).reshape((-1,1)) # N x T
-t_starall = (paddle.tile(t_star, (1, Nx**2)).T).reshape((-1,1))  # N x T
 
 t_star.stop_gradient=False
 x_star.stop_gradient=False
@@ -259,7 +256,7 @@ for k in range(N):
     # Initialize model
     print('Final Time: {}'.format(k + 1))
     w_exact = sol[num_step * k: num_step * (k + 1), :, :]
-    model = PINN(w_exact, layers, M_t, M_x, M_y, state0, t0, t1, n_t, x_star, y_star,x_starall, y_starall,t_starall, tol)
+    model = PINN(w_exact, layers, M_t, M_x, M_y, state0, t0, t1, n_t, x_star, y_star,t_star, tol)
 
     # Train
     for tol in tol_list:
