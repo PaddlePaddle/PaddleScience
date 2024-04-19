e1cdba5816d40ca06dcba68f47dd38359d8fcc74
HydrogenSulfate <490868991@qq.com>
2024-04-19 16:45:04 +0800

    [Docker] Add docker image (#856)
    * execute 'ldconfig' after /bin/bash automatically
    
    * add_dockerhub
    
    * refine doc
    
    * update base image to paddle official image(test=document_fix)

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,9 @@
 docker build . -t paddlescience:latest
 
 if [ -x "$(command -v nvidia-docker)" ]; then
-    nvidia-docker run --network=host -it paddlescience
+    nvidia-docker run --name paddlescience_container --network=host -it paddlescience
 elif [ -x "$(command -v docker)" ]; then
-    docker run --gpus all --network=host -it paddlescience
+    docker run --name paddlescience_container --gpus all --network=host -it paddlescience
 else
     echo "Docker start failed, please install nvidia-docker or docker(>=19.03) first"
 fi
