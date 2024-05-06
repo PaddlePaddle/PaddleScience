c61a5f6a231604f9252214db0fe56421beb0dd97
HydrogenSulfate <490868991@qq.com>
2024-05-06 19:17:26 +0800

    [Fix] Fix inference path typo (#880)
    * fix(test=document_fix)
    
    * fix typo for correcting 'pdpiparmas' to 'pdiparams'
    
    * add left bracket for docstring of CuboidSelfAttentionLayer

--- examples/earthformer/predictor.py
+++ examples/earthformer/predictor.py
@@ -32,7 +32,7 @@ class EarthformerPredictor(base.Predictor):
     ):
         super().__init__(
             cfg.INFER.pdmodel_path,
-            cfg.INFER.pdpiparams_path,
+            cfg.INFER.pdiparams_path,
             device=cfg.INFER.device,
             engine=cfg.INFER.engine,
             precision=cfg.INFER.precision,
