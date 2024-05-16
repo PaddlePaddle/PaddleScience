ded891848c0e36fb3bb25f95d1e20810f40afec5
HydrogenSulfate <490868991@qq.com>
2024-05-16 15:15:11 +0800

    Update deeponet.py

--- examples/operator_learning/deeponet.py
+++ examples/operator_learning/deeponet.py
@@ -254,7 +254,7 @@ def main(cfg: DictConfig):
     elif cfg.mode == "infer":
         inference(cfg)
     else:
-        raise ValueError(f"cfg.mode should in ['train', 'eval'], but got '{cfg.mode}'")
+        raise ValueError(f"cfg.mode should in ['train', 'eval', 'export', 'infer'], but got '{cfg.mode}'")
 
 
 if __name__ == "__main__":
