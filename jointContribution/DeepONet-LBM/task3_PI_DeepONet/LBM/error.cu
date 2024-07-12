281452f1dea3193d316e35e5f24e4c8afb396039
chenkai26 <chenkai26@baidu.com>
2024-07-12 16:12:32 +0800

    DeepONet-LBM

---
+++ jointContribution/DeepONet-LBM/task3_PI_DeepONet/LBM/error.cu
@@ -0,0 +1,21 @@
+#include "lb.h"
+#include "common.h"
+
+double error()
+{
+	int x, y;
+	double sum_u_c = 0.0;
+	double Err;
+
+	for(y = 1; y <= M1; y++)
+		for(x = N16; x < N16+N1; x++)
+		{
+            sum_u_c += UX(y,x);
+		}
+
+	Err = fabs(sum_u_c - sum_u_o)/fabs(sum_u_c);
+	sum_u_o = sum_u_c;
+
+	return Err;
+
+}
