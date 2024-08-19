48993c9766d6247b08891f1dca3788bdb1821c9e
banjiuyufen <52366074+banjiuyufen@users.noreply.github.com>
2024-08-19 17:08:13 +0800

    Add files via upload

--- ppsci/data/dataset/__init__.py
+++ ppsci/data/dataset/__init__.py
@@ -21,6 +21,7 @@ from ppsci.data.dataset.array_dataset import ContinuousNamedArrayDataset
 from ppsci.data.dataset.array_dataset import IterableNamedArrayDataset
 from ppsci.data.dataset.array_dataset import NamedArrayDataset
 from ppsci.data.dataset.atmospheric_dataset import GridMeshAtmosphericDataset
+from ppsci.data.dataset.cgcnn_dataset import CIFData as CGCNNDataset
 from ppsci.data.dataset.csv_dataset import CSVDataset
 from ppsci.data.dataset.csv_dataset import IterableCSVDataset
 from ppsci.data.dataset.cylinder_dataset import MeshCylinderDataset
@@ -79,6 +80,7 @@ __all__ = [
     "ExtMoEENSODataset",
     "SEVIRDataset",
     "build_dataset",
+    "CGCNNDataset",
 ]
 
 
