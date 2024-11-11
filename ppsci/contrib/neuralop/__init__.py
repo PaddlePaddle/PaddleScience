3b23b3f09cba5063adad2220dc637b55829b60b2
xiaoyewww <641311428@qq.com>
2024-11-11 22:17:19 +0800

    feat(neuralop): update fno darcy

--- ppsci/contrib/neuralop/__init__.py
+++ ppsci/contrib/neuralop/__init__.py
@@ -1,3 +1,9 @@
 __version__ = '0.3.0'
 
 from .models import TFNO3d, TFNO2d, TFNO1d, TFNO
+from .models import get_model
+from . import datasets
+from . import mpu
+from .training import Trainer, CheckpointCallback
+from .training import Trainer, CheckpointCallback
+from .losses import LpLoss, H1Loss, BurgersEqnLoss, ICLoss, WeightedSumLoss
---
+++ ppsci/contrib/neuralop/training/__init__.py
@@ -0,0 +1,4 @@
+from .trainer import Trainer
+from .paddle_setup import setup
+from .callbacks import (Callback, BasicLoggerCallback, CheckpointCallback)
+from .load_training_state import load_training_state
