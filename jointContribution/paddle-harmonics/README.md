f7993e3d4e7329f1dfc2e83f06865ec329c0d334
beinggod <beinggod@foxmail.com>
2024-11-11 13:08:42 +0800

    [Feat] Add paddle-harmonics

---
+++ jointContribution/paddle-harmonics/README.md
@@ -0,0 +1,246 @@
+<!--
+SPDX-FileCopyrightText: Copyright (c) 2022 The torch-harmonics Authors. All rights reserved.
+
+SPDX-License-Identifier: BSD-3-Clause
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-->
+
+<!-- <div align="center">
+    <img src="https://raw.githubusercontent.com/NVIDIA/torch-harmonics/main/images/logo/logo.png"  width="568">
+    <br>
+    <a href="https://github.com/NVIDIA/torch-harmonics/actions/workflows/tests.yml"><img src="https://github.com/NVIDIA/torch-harmonics/actions/workflows/tests.yml/badge.svg"></a>
+    <a href="https://pypi.org/project/torch_harmonics/"><img src="https://img.shields.io/pypi/v/torch_harmonics"></a>
+</div> -->
+
+<!--
+[![pypi](https://img.shields.io/pypi/v/torch_harmonics)](https://pypi.org/project/torch_harmonics/)
+-->
+
+<!-- # spherical harmonic transforms -->
+
+# paddle-harmonics
+
+![paddle_logo](images/logo/paddle_logo.png)
+
+> [!IMPORTANT]
+> paddle-harmonics is develop base [torch-harmonics](https://github.com/NVIDIA/torch-harmonics/tree/v0.6.5).
+>
+> It is recommended to install **nightly-build(develop)** Paddle before running any code in this repo.
+
+[**Overview**](#overview) | [**Installation**](#installation) | [**More information**](#more-about-torch-harmonics) | [**Getting started**](#getting-started) | [**Contributors**](#contributors) | [**Cite us**](#cite-us) | [**References**](#references)
+
+
+## Overview
+
+paddle-harmonics is a differentiable implementation of the Spherical Harmonic transform in Paddle. It was originally implemented to enable Spherical Fourier Neural Operators (SFNO). It uses quadrature rules to compute the projection onto the associated Legendre polynomials and FFTs for the projection onto the harmonic basis. This algorithm tends to outperform others with better asymptotic scaling for most practical purposes.
+
+paddle-harmonics uses Paddle primitives to implement these operations, making it fully differentiable. Moreover, the quadrature can be distributed onto multiple ranks making it spatially distributed.
+
+paddle-harmonics has been used to implement a variety of differentiable PDE solvers which generated the animations below. Moreover, it has enabled the development of Spherical Fourier Neural Operators (SFNOs) [1].
+
+<div align="center">
+<table border="0" cellspacing="0" cellpadding="0">
+    <tr>
+        <td><img src="https://media.githubusercontent.com/media/NVIDIA/torch-harmonics/main/images/sfno.gif"  width="240"></td>
+        <td><img src="https://media.githubusercontent.com/media/NVIDIA/torch-harmonics/main/images/zonal_jet.gif"  width="240"></td>
+        <td><img src="https://media.githubusercontent.com/media/NVIDIA/torch-harmonics/main/images/allen-cahn.gif"  width="240"></td>
+    </tr>
+<!--     <tr>
+        <td style="text-align:center; border-style : hidden!important;">Shallow Water Eqns.</td>
+        <td style="text-align:center; border-style : hidden!important;">Ginzburg-Landau Eqn.</td>
+        <td style="text-align:center; border-style : hidden!important;">Allen-Cahn Eqn.</td>
+    </tr>  -->
+</table>
+</div>
+
+
+## Installation
+Build in your environment using the Python package:
+
+```bash
+git clone git@github.com:PFCCLab/paddle_harmonics.git
+cd paddle-harmonics
+pip install -e .
+```
+
+## More about paddle-harmonics
+
+### Spherical harmonics
+
+The [spherical harmonics](https://en.wikipedia.org/wiki/Spherical_harmonics) are special functions defined on the two-dimensional sphere $S^2$ (embedded in three dimensions). They form an orthonormal basis of the space of square-integrable functions defined on the sphere $L^2(S^2)$ and are comparable to the harmonic functions defined on a circle/torus. The spherical harmonics are defined as
+
+$$
+Y_l^m(\theta, \lambda) = \sqrt{\frac{(2l + 1)}{4 \pi} \frac{(l - m)!}{(l + m)!}} P_l^m(\cos \theta)  \exp(im\lambda),
+$$
+
+where $\theta$ and $\lambda$ are colatitude and longitude respectively, and $P_l^m$ the normalized, [associated Legendre polynomials](https://en.wikipedia.org/wiki/Associated_Legendre_polynomials).
+
+<div align="center">
+<img src="https://media.githubusercontent.com/media/NVIDIA/torch-harmonics/main/images/spherical_harmonics.gif" width="432">
+<br>
+Spherical harmonics up to degree 5
+</div>
+
+### Spherical harmonic transform
+
+The spherical harmonic transform (SHT)
+
+$$
+f_l^m = \int_{S^2}  \overline{Y_{l}^{m}}(\theta, \lambda) f(\theta, \lambda) \mathrm{d} \mu(\theta, \lambda)
+$$
+
+realizes the projection of a signal $f(\theta, \lambda)$ on $S^2$ onto the spherical harmonics basis. The SHT generalizes the Fourier transform on the sphere. Conversely, a truncated series expansion of a function $f$ can be written in terms of spherical harmonics as
+
+$$
+f (\theta, \lambda) = \sum_{m=-M}^{M} \exp(im\lambda) \sum_{l=|m|}^{M} \hat f_l^m  P_l^m (\cos \theta),
+$$
+
+where $\hat{f}_l^m$, are the expansion coefficients associated to the mode $m$, $n$.
+
+The implementation of the SHT follows the algorithm as presented in [2]. A direct spherical harmonic transform can be accomplished by a Fourier transform
+
+$$
+\hat f^m(\theta) = \frac{1}{2 \pi} \int_{0}^{2\pi} f(\theta, \lambda) \exp(-im\lambda) \mathrm{d} \lambda
+$$
+
+in longitude and a Legendre transform
+
+$$
+\hat f_l^m = \frac{1}{2} \int^{\pi}_0 \hat f^{m} (\theta) P_l^m (\cos \theta) \sin \theta \mathrm{d} \theta
+$$
+
+in latitude.
+
+### Discrete Legendre transform
+
+The second integral, which computed the projection onto the Legendre polynomials is realized with quadrature. On the Gaussian grid, we use Gaussian quadrature in the $\cos \theta$ domain. The integral
+
+$$
+\hat f_l^m = \frac{1}{2} \int_{-1}^1 \hat{f}^m(\arccos x) P_l^m (x) \mathrm{d} x
+$$
+
+is obtained with the substitution $x = \cos \theta$ and then approximated by the sum
+
+$$
+\hat f_l^m = \sum_{j=1}^{N_\theta}  \hat{f}^m(\arccos x_j) P_l^m(x_j) w_j.
+$$
+
+Here, $x_j \in [-1,1]$ are the quadrature nodes with the respective quadrature weights $w_j$.
+
+## Getting started
+
+The main functionality of `paddle_harmonics` is provided in the form of `paddle.nn.Layers` for composability. A minimum example is given by:
+
+```python
+import paddle
+import paddle_harmonics as th
+
+device = paddle.device.set_device('gpu' if paddle.device.cuda.device_count() > 0 else 'cpu')
+
+nlat = 512
+nlon = 2*nlat
+batch_size = 32
+signal = paddle.randn([batch_size, nlat, nlon])
+
+# transform data on an equiangular grid
+sht = th.RealSHT(nlat, nlon, grid="equiangular").to(device)
+
+coeffs = sht(signal)
+```
+
+Detailed usage of paddle-harmonics, alongside helpful analysis provided in a series of notebooks:
+
+1. [Getting started](./notebooks/getting_started.ipynb)
+2. [Quadrature](./notebooks/quadrature.ipynb)
+3. [Visualizing the spherical harmonics](./notebooks/plot_spherical_harmonics.ipynb)
+4. [Spectral fitting vs. SHT](./notebooks/gradient_analysis.ipynb)
+5. [Conditioning of the Gramian](./notebooks/conditioning_sht.ipynb)
+6. [Solving the Helmholtz equation](./notebooks/helmholtz.ipynb)
+7. [Solving the shallow water equations](./notebooks/shallow_water_equations.ipynb)
+8. [Training Spherical Fourier Neural Operators](./notebooks/train_sfno.ipynb)
+
+## Remarks on automatic mixed precision (AMP) support
+
+Note that paddle-harmonics uses Fourier transforms from `paddle.fft` which in turn uses kernels from the optimized `cuFFT` library. This library supports fourier transforms of `float32` and `float64` (i.e. `single` and `double` precision) tensors for all input sizes. For `float16` (i.e. `half` precision) and `bfloat16` inputs however, the dimensions which are transformed are restricted to powers of two. Since data is converted to one of these reduced precision floating point formats when `paddle.amp.auto_cast` is used, paddle-harmonics will issue an error when the input shapes are not powers of two. For these cases, we recommend disabling auto_cast for the harmonics transform specifically:
+
+```python
+import paddle
+import paddle_harmonics as th
+
+sht = th.RealSHT(512, 1024, grid="equiangular").to('gpu')
+
+with paddle.amp.auto_cast():
+   # do some AMP converted math here
+   x = some_math(x)
+   # convert tensor to float32
+   x = x.to(paddle.float32)
+   # now disable auto_cast specifically for the transform,
+   # making sure that the tensors are not converted
+   # back to reduced precision internally
+   with paddle.amp.auto_cast(enabled = False):
+      xt = sht(x)
+
+   # continue operating on the transformed tensor
+   xt = some_more_math(xt)
+```
+
+Depending on the problem, it might be beneficial to upcast data to `float64` instead of `float32` precision for numerical stability.
+
+## Contributors
+
+[Boris Bonev](https://bonevbs.github.io) (bbonev@nvidia.com), [Thorsten Kurth](https://github.com/azrael417) (tkurth@nvidia.com), [Christian Hundt](https://github.com/gravitino) (chundt@nvidia.com), [Nikola Kovachki](https://kovachki.github.io) (nkovachki@nvidia.com), [Jean Kossaifi](http://jeankossaifi.com) (jkossaifi@nvidia.com)  
+
+## Cite us
+
+If you use `paddle-harmonics` in an academic paper, please cite [1]
+
+```bibtex
+@misc{bonev2023spherical,
+      title={Spherical Fourier Neural Operators: Learning Stable Dynamics on the Sphere},
+      author={Boris Bonev and Thorsten Kurth and Christian Hundt and Jaideep Pathak and Maximilian Baust and Karthik Kashinath and Anima Anandkumar},
+      year={2023},
+      eprint={2306.03838},
+      archivePrefix={arXiv},
+      primaryClass={cs.LG}
+}
+```
+
+## References
+
+<a id="1">[1]</a>
+Bonev B., Kurth T., Hundt C., Pathak, J., Baust M., Kashinath K., Anandkumar A.;
+Spherical Fourier Neural Operators: Learning Stable Dynamics on the Sphere;
+arXiv 2306.0383, 2023.
+
+<a id="1">[2]</a>
+Schaeffer N.;
+Efficient spherical harmonic transforms aimed at pseudospectral numerical simulations;
+G3: Geochemistry, Geophysics, Geosystems, 2013.
+
+<a id="1">[3]</a>
+Wang B., Wang L., Xie Z.;
+Accurate calculation of spherical and vector spherical harmonic expansions via spectral element grids;
+Adv Comput Math, 2018.
---
+++ jointContribution/paddle-harmonics/paddle_harmonics/examples/shallow_water_equations.py
@@ -0,0 +1,451 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: Copyright (c) 2022 The torch-harmonics Authors. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+
+import numpy as np
+import paddle
+import paddle.nn as nn
+import paddle_harmonics as harmonics
+
+
+def complex_sqrt(x):
+    assert x.is_complex(), "x must be a complex tensor."
+    x_real = x.real()
+    x_imag = x.imag()
+
+    modulus = paddle.sqrt(x_real**2 + x_imag**2)
+
+    y_real = paddle.sqrt((modulus + x_real) / 2)
+    y_imag = paddle.sign(x_imag) * paddle.sqrt((modulus - x_real) / 2)
+
+    return paddle.stack([y_real, y_imag], axis=-1).contiguous().as_complex()
+
+
+class ShallowWaterSolver(nn.Layer):
+    """
+    SWE solver class. Interface inspired bu pyspharm and SHTns
+    """
+
+    def __init__(
+        self,
+        nlat,
+        nlon,
+        dt,
+        lmax=None,
+        mmax=None,
+        grid="legendre-gauss",
+        radius=6.37122e6,
+        omega=7.292e-5,
+        gravity=9.80616,
+        havg=10.0e3,
+        hamp=120.0,
+    ):
+        super().__init__()
+
+        # time stepping param
+        self.dt = dt
+
+        # grid parameters
+        self.nlat = nlat
+        self.nlon = nlon
+        self.grid = grid
+
+        # physical sonstants
+        self.register_buffer("radius", paddle.to_tensor([radius], dtype=paddle.float64))
+        self.register_buffer("omega", paddle.to_tensor([omega], dtype=paddle.float64))
+        self.register_buffer(
+            "gravity", paddle.to_tensor([gravity], dtype=paddle.float64)
+        )
+        self.register_buffer("havg", paddle.to_tensor([havg], dtype=paddle.float64))
+        self.register_buffer("hamp", paddle.to_tensor([hamp], dtype=paddle.float64))
+
+        # SHT
+        self.sht = harmonics.RealSHT(
+            nlat, nlon, lmax=lmax, mmax=mmax, grid=grid, csphase=False
+        )
+        self.isht = harmonics.InverseRealSHT(
+            nlat, nlon, lmax=lmax, mmax=mmax, grid=grid, csphase=False
+        )
+        self.vsht = harmonics.RealVectorSHT(
+            nlat, nlon, lmax=lmax, mmax=mmax, grid=grid, csphase=False
+        )
+        self.ivsht = harmonics.InverseRealVectorSHT(
+            nlat, nlon, lmax=lmax, mmax=mmax, grid=grid, csphase=False
+        )
+
+        self.lmax = lmax or self.sht.lmax
+        self.mmax = lmax or self.sht.mmax
+
+        # compute gridpoints
+        if self.grid == "legendre-gauss":
+            cost, quad_weights = harmonics.quadrature.legendre_gauss_weights(
+                self.nlat, -1, 1
+            )
+        elif self.grid == "lobatto":
+            cost, quad_weights = harmonics.quadrature.lobatto_weights(self.nlat, -1, 1)
+        elif self.grid == "equiangular":
+            cost, quad_weights = harmonics.quadrature.clenshaw_curtiss_weights(
+                self.nlat, -1, 1
+            )
+
+        quad_weights = paddle.to_tensor(quad_weights).reshape([-1, 1])
+
+        # apply cosine transform and flip them
+        lats = -paddle.to_tensor(np.arcsin(cost))
+        lons = paddle.linspace(0, 2 * np.pi, self.nlon + 1, dtype=paddle.float64)[:nlon]
+
+        self.lmax = self.sht.lmax
+        self.mmax = self.sht.mmax
+
+        # compute the laplace and inverse laplace operators
+        l = paddle.arange(0, self.lmax).reshape([self.lmax, 1]).to(paddle.float64)
+        l = l.expand([self.lmax, self.mmax])
+        # the laplace operator acting on the coefficients is given by - l (l + 1)
+        lap = -l * (l + 1) / self.radius**2
+        invlap = -self.radius**2 / l / (l + 1)
+        invlap[0] = 0.0
+
+        # compute coriolis force
+        coriolis = 2 * self.omega * paddle.sin(lats).reshape([self.nlat, 1])
+
+        # hyperdiffusion
+        hyperdiff = paddle.exp(
+            paddle.to_tensor((-self.dt / 2 / 3600.0) * (lap / lap[-1, 0]) ** 4)
+        )
+
+        # register all
+        self.register_buffer("lats", lats)
+        self.register_buffer("lons", lons)
+        self.register_buffer("l", l)
+        self.register_buffer("lap", lap)
+        self.register_buffer("invlap", invlap)
+        self.register_buffer("coriolis", coriolis)
+        self.register_buffer("hyperdiff", hyperdiff)
+        self.register_buffer("quad_weights", quad_weights)
+
+    def grid2spec(self, ugrid):
+        """
+        spectral coefficients from spatial data
+        """
+        return self.sht(ugrid)
+
+    def spec2grid(self, uspec):
+        """
+        spatial data from spectral coefficients
+        """
+        return self.isht(uspec)
+
+    def vrtdivspec(self, ugrid):
+        """spatial data from spectral coefficients"""
+        vrtdivspec = self.lap * self.radius * self.vsht(ugrid)
+        return vrtdivspec
+
+    def getuv(self, vrtdivspec):
+        """
+        compute wind vector from spectral coeffs of vorticity and divergence
+        """
+        return self.ivsht(self.invlap * vrtdivspec / self.radius)
+
+    def gethuv(self, uspec):
+        """
+        compute wind vector from spectral coeffs of vorticity and divergence
+        """
+        hgrid = self.spec2grid(uspec[:1])
+        uvgrid = self.getuv(uspec[1:])
+        return paddle.concat((hgrid, uvgrid), axis=-3)
+
+    def potential_vorticity(self, uspec):
+        """
+        Compute potential vorticity
+        """
+        ugrid = self.spec2grid(uspec)
+        pvrt = (
+            (0.5 * self.havg * self.gravity / self.omega)
+            * (ugrid[1] + self.coriolis)
+            / ugrid[0]
+        )
+        return pvrt
+
+    def dimensionless(self, uspec):
+        """
+        Remove dimensions from variables
+        """
+        uspec[0] = (uspec[0] - self.havg * self.gravity) / self.hamp / self.gravity
+        # vorticity is measured in 1/s so we normalize using sqrt(g h) / r
+        uspec[1:] = uspec[1:] * self.radius / paddle.sqrt(self.gravity * self.havg)
+        return uspec
+
+    def dudtspec(self, uspec):
+        """
+        Compute time derivatives from solution represented in spectral coefficients
+        """
+
+        dudtspec = paddle.zeros_like(uspec)
+
+        # compute the derivatives - this should be incorporated into the solver:
+        ugrid = self.spec2grid(uspec)
+        uvgrid = self.getuv(uspec[1:])
+
+        # phi = ugrid[0]
+        # vrtdiv = ugrid[1:]
+
+        tmp = uvgrid * (ugrid[1] + self.coriolis)
+        tmpspec = self.vrtdivspec(tmp)
+        dudtspec[2] = tmpspec[0]
+        dudtspec[1] = -1 * tmpspec[1]
+
+        tmp = uvgrid * ugrid[0]
+        tmp = self.vrtdivspec(tmp)
+        dudtspec[0] = -1 * tmp[1]
+
+        tmpspec = self.grid2spec(ugrid[0] + 0.5 * (uvgrid[0] ** 2 + uvgrid[1] ** 2))
+        dudtspec[2] = dudtspec[2] - self.lap * tmpspec
+
+        return dudtspec
+
+    def galewsky_initial_condition(self):
+        """
+        Initializes non-linear barotropically unstable shallow water test case of Galewsky et al. (2004, Tellus, 56A, 429-440).
+
+        [1] Galewsky; An initial-value problem for testing numerical models of the global shallow-water equations;
+            DOI: 10.1111/j.1600-0870.2004.00071.x; http://www-vortex.mcs.st-and.ac.uk/~rks/reprints/galewsky_etal_tellus_2004.pdf
+        """
+        place = self.lap.place
+
+        umax = 80.0
+        phi0 = paddle.to_tensor([paddle.pi / 7.0], place=place)
+        phi1 = paddle.to_tensor([0.5 * paddle.pi - phi0], place=place)
+        phi2 = 0.25 * paddle.pi
+        en = paddle.exp(paddle.to_tensor(-4.0 / (phi1 - phi0) ** 2, place=place))
+        alpha = 1.0 / 3.0
+        beta = 1.0 / 15.0
+
+        lats, lons = paddle.meshgrid(self.lats, self.lons)
+
+        u1 = (umax / en) * paddle.exp(1.0 / ((lats - phi0) * (lats - phi1)))
+        ugrid = paddle.where(
+            paddle.logical_and(lats < phi1, lats > phi0),
+            u1,
+            paddle.zeros((self.nlat, self.nlon)).to(place),
+        )
+        vgrid = paddle.zeros((self.nlat, self.nlon)).to(place)
+        hbump = (
+            self.hamp
+            * paddle.cos(lats)
+            * paddle.exp(-(((lons - paddle.pi) / alpha) ** 2))
+            * paddle.exp(-((phi2 - lats) ** 2) / beta)
+        )
+
+        # intial velocity field
+        vgrid = vgrid.to(ugrid.dtype)
+        ugrid = paddle.stack((ugrid, vgrid))
+        # intial vorticity/divergence field
+        vrtdivspec = self.vrtdivspec(ugrid)
+        vrtdivgrid = self.spec2grid(vrtdivspec)
+
+        # solve balance eqn to get initial zonal geopotential with a localized bump (not balanced).
+        tmp = ugrid * (vrtdivgrid + self.coriolis)
+        tmpspec = self.vrtdivspec(tmp)
+        tmpspec[1] = self.grid2spec(0.5 * paddle.sum(ugrid**2, axis=0))
+        phispec = (
+            self.invlap * tmpspec[0]
+            - tmpspec[1]
+            + self.grid2spec(self.gravity * (self.havg + hbump))
+        )
+
+        # assemble solution
+        uspec = paddle.zeros([3, self.lmax, self.mmax], dtype=vrtdivspec.dtype).to(
+            place
+        )
+        uspec[0] = phispec
+        uspec[1:] = vrtdivspec
+
+        return paddle.tril(uspec)
+
+    def random_initial_condition(self, mach=0.1) -> paddle.Tensor:
+        """
+        random initial condition on the sphere
+        """
+        place = self.lap.place
+        ctype = (
+            paddle.complex128 if self.lap.dtype == paddle.float64 else paddle.complex64
+        )
+
+        # mach number relative to wave speed
+        llimit = mlimit = 80
+
+        # initial geopotential
+        uspec = paddle.zeros([3, self.lmax, self.mmax], dtype=ctype).to(place)
+        uspec[:, :llimit, :mlimit] = complex_sqrt(
+            paddle.to_tensor(
+                4 * paddle.pi / llimit / (llimit + 1), place=place, dtype=ctype
+            )
+        ) * paddle.randn(uspec[:, :llimit, :mlimit].shape, dtype=uspec.dtype)
+
+        uspec[0] = self.gravity * self.hamp * uspec[0]
+        uspec[0, 0, 0] += (
+            complex_sqrt(paddle.to_tensor(4 * paddle.pi, place=place, dtype=ctype))
+            * self.havg
+            * self.gravity
+        )
+        uspec[1:] = (
+            mach * uspec[1:] * paddle.sqrt(self.gravity * self.havg) / self.radius
+        )
+
+        return paddle.tril(uspec)
+
+    def timestep(self, uspec: paddle.Tensor, nsteps: int) -> paddle.Tensor:
+        """
+        Integrate the solution using Adams-Bashforth / forward Euler for nsteps steps.
+        """
+
+        dudtspec = paddle.zeros([3, 3, self.lmax, self.mmax], dtype=uspec.dtype).to(
+            uspec.place
+        )
+
+        # pointers to indicate the most current result
+        inew = 0
+        inow = 1
+        iold = 2
+
+        for iter in range(nsteps):
+            dudtspec[inew] = self.dudtspec(uspec)
+
+            # update vort,div,phiv with third-order adams-bashforth.
+            # forward euler, then 2nd-order adams-bashforth time steps to start.
+            if iter == 0:
+                dudtspec[inow] = dudtspec[inew]
+                dudtspec[iold] = dudtspec[inew]
+            elif iter == 1:
+                dudtspec[iold] = dudtspec[inew]
+
+            uspec = uspec + self.dt * (
+                (23.0 / 12.0) * dudtspec[inew]
+                - (16.0 / 12.0) * dudtspec[inow]
+                + (5.0 / 12.0) * dudtspec[iold]
+            )
+
+            # implicit hyperdiffusion for vort and div.
+            uspec[1:] = self.hyperdiff * uspec[1:]
+
+            # cycle through the indices
+            inew = (inew - 1) % 3
+            inow = (inow - 1) % 3
+            iold = (iold - 1) % 3
+
+        return uspec
+
+    def integrate_grid(self, ugrid, dimensionless=False, polar_opt=0):
+        dlon = 2 * paddle.pi / self.nlon
+        radius = 1 if dimensionless else self.radius
+        if polar_opt > 0:
+            out = paddle.sum(
+                ugrid[..., polar_opt:-polar_opt, :]
+                * self.quad_weights[polar_opt:-polar_opt]
+                * dlon
+                * radius**2,
+                axis=(-2, -1),
+            )
+        else:
+            out = paddle.sum(
+                ugrid * self.quad_weights * dlon * radius**2, axis=(-2, -1)
+            )
+        return out
+
+    def plot_griddata(
+        self,
+        data,
+        fig,
+        cmap="twilight_shifted",
+        vmax=None,
+        vmin=None,
+        projection="3d",
+        title=None,
+        antialiased=False,
+    ):
+        """
+        plotting routine for data on the grid. Requires cartopy for 3d plots.
+        """
+        import matplotlib.pyplot as plt
+
+        lons = self.lons.squeeze() - paddle.pi
+        lats = self.lats.squeeze()
+
+        if data.place.is_gpu_place():
+            data = data.numpy()
+            lons = lons.numpy()
+            lats = lats.numpy()
+
+        Lons, Lats = np.meshgrid(lons, lats)
+
+        if projection == "mollweide":
+
+            # ax = plt.gca(projection=projection)
+            ax = fig.add_subplot(projection=projection)
+            im = ax.pcolormesh(Lons, Lats, data, cmap=cmap, vmax=vmax, vmin=vmin)
+            # ax.set_title("Elevation map of mars")
+            ax.grid(True)
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
+            plt.colorbar(im, orientation="horizontal")
+            plt.title(title)
+
+        elif projection == "3d":
+
+            import cartopy.crs as ccrs
+
+            proj = ccrs.Orthographic(central_longitude=0.0, central_latitude=25.0)
+
+            # ax = plt.gca(projection=proj, frameon=True)
+            ax = fig.add_subplot(projection=proj)
+            Lons = Lons * 180 / np.pi
+            Lats = Lats * 180 / np.pi
+
+            # contour data over the map.
+            im = ax.pcolormesh(
+                Lons,
+                Lats,
+                data,
+                cmap=cmap,
+                transform=ccrs.PlateCarree(),
+                antialiased=antialiased,
+                vmax=vmax,
+                vmin=vmin,
+            )
+            plt.title(title, y=1.05)
+
+        else:
+            raise NotImplementedError
+
+        return im
+
+    def plot_specdata(self, data, fig, **kwargs):
+        return self.plot_griddata(self.isht(data), fig, **kwargs)
