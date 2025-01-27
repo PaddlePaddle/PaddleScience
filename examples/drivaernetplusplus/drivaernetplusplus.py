beb8e38195607be8ab9807921a8e1184a2902506
Lil_Ken <98253413+LilaKen@users.noreply.github.com>
2025-01-28 01:07:25 +0800

    Update examples/drivaernetplusplus/drivaernetplusplus.py
    Co-authored-by: HydrogenSulfate <490868991@qq.com>

--- examples/drivaernetplusplus/drivaernetplusplus.py
+++ examples/drivaernetplusplus/drivaernetplusplus.py
@@ -146,9 +146,10 @@ def evaluate(cfg: DictConfig):
         "dataset": {
             "name": "DrivAerNetPlusPlusDataset",
             "root_dir": cfg.ARGS.dataset_path,
-            "input_keys": (cfg.MODEL.input_keys),
-            "label_keys": (cfg.MODEL.output_keys),
-            "weight_keys": (cfg.MODEL.weight_keys),
+            "input_keys": cfg.MODEL.input_keys,
+            "label_keys": cfg.MODEL.output_keys,
+            "weight_keys": cfg.MODEL.weight_keys,
+
             "subset_dir": cfg.ARGS.subset_dir,
             "ids_file": cfg.EVAL.ids_file,
             "csv_file": cfg.ARGS.aero_coeff,
