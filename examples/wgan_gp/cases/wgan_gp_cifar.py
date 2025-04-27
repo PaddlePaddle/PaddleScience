0d7b0b031c1aefe32ed141164caff21ff3fb9de0
Biaolin Wen <robinbg@foxmail.com>
2025-04-28 00:26:17 +0800

    Update wgan_gp_cifar.py

--- examples/wgan_gp/cases/wgan_gp_cifar.py
+++ examples/wgan_gp/cases/wgan_gp_cifar.py
@@ -6,7 +6,7 @@ import paddle.nn as nn
 import paddle.vision.transforms as transforms
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+import models
 from models.wgan_gp import WGAN_GP
 
 class CIFAR10Generator(nn.Layer):
