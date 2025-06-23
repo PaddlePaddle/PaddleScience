859972471688e56b49ff76e055ef500148e84401
dylan-yin <52811073+dylan-yin@users.noreply.github.com>
2025-06-23 18:12:26 +0800

    Update stafnet.py

--- ppsci/arch/stafnet.py
+++ ppsci/arch/stafnet.py
@@ -6,13 +6,9 @@ import numpy as np
 import paddle
 from ppsci.arch import base
 try:
-    from pgl.nn.conv import GATv2Conv
-except ModuleNotFoundError as e:
-    logger.error("please install pgl with \'pip install pgl\'.") 
-    raise ModuleNotFoundError(str(e))
-    
-
-
+    import pgl
+except ModuleNotFoundError:
+    pass
 
 class Inception_Block_V1(paddle.nn.Layer):
     def __init__(self, in_channels, out_channels, num_kernels=6, init_weight=True):
