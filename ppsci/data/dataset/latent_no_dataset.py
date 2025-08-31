940f30c8a7327afbafce68fac77be448555fc80e
Dubhe Chang <zhangshulun21@mails.ucas.ac.cn>
2025-08-31 22:29:53 +0800

    update latent_no_dataset.py

--- ppsci/data/dataset/latent_no_dataset.py
+++ ppsci/data/dataset/latent_no_dataset.py
@@ -1,12 +1,77 @@
 import os
+import os.path as osp
 from typing import Callable
 from typing import Dict
 from typing import Optional
 from typing import Tuple
 
 import numpy as np
-import paddle
 from paddle import io
+
+
+class Normalizer:
+    def __init__(self, x: np.ndarray, y1: np.ndarray, y2: np.ndarray):
+        self.x_flag = False
+        self.y1_flag = False
+        self.y2_flag = False
+
+        old_x_shape = x.shape
+        old_y1_shape = y1.shape
+        old_y2_shape = y2.shape
+
+        x = x.reshape(-1, x.shape[-1])
+        y1 = y1.reshape(-1, y1.shape[-1])
+        y2 = y2.reshape(-1, y2.shape[-1])
+
+        self.x_mean = np.mean(x, axis=0, keepdims=True).astype("float32")
+        self.x_std = (np.std(x, axis=0, keepdims=True) + 1e-8).astype("float32")
+        self.y1_mean = np.mean(y1, axis=0, keepdims=True).astype("float32")
+        self.y1_std = (np.std(y1, axis=0, keepdims=True) + 1e-8).astype("float32")
+        self.y2_mean = np.mean(y2, axis=0, keepdims=True).astype("float32")
+        self.y2_std = (np.std(y2, axis=0, keepdims=True) + 1e-8).astype("float32")
+
+        x = x.reshape(old_x_shape)
+        y1 = y1.reshape(old_y1_shape)
+        y2 = y2.reshape(old_y2_shape)
+
+    def is_apply_x(self) -> bool:
+        return self.x_flag
+
+    def is_apply_y1(self) -> bool:
+        return self.y1_flag
+
+    def is_apply_y2(self) -> bool:
+        return self.y2_flag
+
+    def apply_x(self, x: np.ndarray, inverse: bool = False) -> np.ndarray:
+        old_shape = x.shape
+        x = x.reshape(-1, x.shape[-1])
+        if not inverse:
+            x = (x - self.x_mean) / self.x_std
+            self.x_flag = True
+        else:
+            x = x * self.x_std + self.x_mean
+        return x.reshape(old_shape).astype("float32")
+
+    def apply_y1(self, y1: np.ndarray, inverse: bool = False) -> np.ndarray:
+        old_shape = y1.shape
+        y1 = y1.reshape(-1, y1.shape[-1])
+        if not inverse:
+            y1 = (y1 - self.y1_mean) / self.y1_std
+            self.y1_flag = True
+        else:
+            y1 = y1 * self.y1_std + self.y1_mean
+        return y1.reshape(old_shape).astype("float32")
+
+    def apply_y2(self, y2: np.ndarray, inverse: bool = False) -> np.ndarray:
+        old_shape = y2.shape
+        y2 = y2.reshape(-1, y2.shape[-1])
+        if not inverse:
+            y2 = (y2 - self.y2_mean) / self.y2_std
+            self.y2_flag = True
+        else:
+            y2 = y2 * self.y2_std + self.y2_mean
+        return y2.reshape(old_shape).astype("float32")
 
 
 class LatentNODataset(io.Dataset):
@@ -25,78 +90,6 @@ class LatentNODataset(io.Dataset):
 
     # Whether support batch indexing for speeding up fetching process.
     batch_index: bool = True
-
-    class normalizer:
-        def __init__(self, x, y1, y2):
-            self.x_flag = False
-            self.y1_flag = False
-            self.y2_flag = False
-            old_x_shape = x.shape
-            old_y1_shape = y1.shape
-            old_y2_shape = y2.shape
-            x = paddle.reshape(x, (-1, x.shape[-1]))
-            y1 = paddle.reshape(y1, (-1, y1.shape[-1]))
-            y2 = paddle.reshape(y2, (-1, y2.shape[-1]))
-            self.x_mean = paddle.mean(x, axis=0)
-            self.x_std = paddle.std(x, axis=0) + 1e-8
-            self.y1_mean = paddle.mean(y1, axis=0)
-            self.y1_std = paddle.std(y1, axis=0) + 1e-8
-            self.y2_mean = paddle.mean(y2, axis=0)
-            self.y2_std = paddle.std(y2, axis=0) + 1e-8
-            x = paddle.reshape(x, old_x_shape)
-            y1 = paddle.reshape(y1, old_y1_shape)
-            y2 = paddle.reshape(y2, old_y2_shape)
-
-        def is_apply_x(self):
-            return self.x_flag
-
-        def is_apply_y1(self):
-            return self.y1_flag
-
-        def is_apply_y2(self):
-            return self.y2_flag
-
-        def apply_x(self, x, device, inverse=False):
-            self.x_mean = self.x_mean.to(device)
-            self.x_std = self.x_std.to(device)
-
-            old_x_shape = x.shape
-            x = paddle.reshape(x, (-1, x.shape[-1]))
-            if not inverse:
-                x = (x - self.x_mean) / self.x_std
-                self.x_flag = True
-            else:
-                x = x * self.x_std + self.x_mean
-            x = paddle.reshape(x, old_x_shape)
-            return x
-
-        def apply_y1(self, y1, device, inverse=False):
-            self.y1_mean = self.y1_mean.to(device)
-            self.y1_std = self.y1_std.to(device)
-
-            old_y1_shape = y1.shape
-            y1 = paddle.reshape(y1, (-1, y1.shape[-1]))
-            if not inverse:
-                y1 = (y1 - self.y1_mean) / self.y1_std
-                self.y1_flag = True
-            else:
-                y1 = y1 * self.y1_std + self.y1_mean
-            y1 = paddle.reshape(y1, old_y1_shape)
-            return y1
-
-        def apply_y2(self, y2, device, inverse=False):
-            self.y2_mean = self.y2_mean.to(device)
-            self.y2_std = self.y2_std.to(device)
-
-            old_y2_shape = y2.shape
-            y2 = paddle.reshape(y2, (-1, y2.shape[-1]))
-            if not inverse:
-                y2 = (y2 - self.y2_mean) / self.y2_std
-                self.y2_flag = True
-            else:
-                y2 = y2 * self.y2_std + self.y2_mean
-            y2 = paddle.reshape(y2, old_y2_shape)
-            return y2
 
     def __init__(
         self,
@@ -120,30 +113,31 @@ class LatentNODataset(io.Dataset):
         self.transform_fn = transform_fn
 
         # Load data
-        data_file = f"./datas/{data_name}_{data_mode}.npy"
+        data_file = osp.join("datas", f"{data_name}_{data_mode}.npy")
         if not os.path.exists(data_file):
             raise FileNotFoundError(f"Data file not found: {data_file}")
 
         dataset = np.load(data_file, allow_pickle=True).tolist()
 
-        x = paddle.to_tensor(dataset["x"], dtype="float32")
-        y1 = paddle.to_tensor(dataset["y1"], dtype="float32")
-        y2 = paddle.to_tensor(dataset["y2"], dtype="float32")
-        x = paddle.reshape(x, (x.shape[0], -1, x.shape[-1]))
-        y1 = paddle.reshape(y1, (y1.shape[0], -1, y1.shape[-1]))
-        y2 = paddle.reshape(y2, (y2.shape[0], -1, y2.shape[-1]))
+        x = dataset["x"].astype("float32")
+        y1 = dataset["y1"].astype("float32")
+        y2 = dataset["y2"].astype("float32")
+
+        x = x.reshape((x.shape[0], -1, x.shape[-1]))
+        y1 = y1.reshape((y1.shape[0], -1, y1.shape[-1]))
+        y2 = y2.reshape((y2.shape[0], -1, y2.shape[-1]))
         # Concatenate x and y1 if required
         if data_concat:
-            y1 = paddle.concat((x, y1), axis=-1)
+            y1 = np.concatenate((x, y1), axis=-1)
 
         # Initialize normalizer
-        self.normalizer = LatentNODataset.normalizer(x, y1, y2)
+        self.normalizer = Normalizer(x, y1, y2)
 
         # Apply normalization if required
         if data_normalize:
-            x = self.normalizer.apply_x(x, "cpu")
-            y1 = self.normalizer.apply_y1(y1, "cpu")
-            y2 = self.normalizer.apply_y2(y2, "cpu")
+            x = self.normalizer.apply_x(x)
+            y1 = self.normalizer.apply_y1(y1)
+            y2 = self.normalizer.apply_y2(y2)
 
         # Store data in input and label dictionaries following PaddleScience convention
         self.input = {
@@ -158,11 +152,6 @@ class LatentNODataset(io.Dataset):
         self._length = x.shape[0]
 
     def __getitem__(self, index: int):
-        """Get item following PaddleScience convention.
-
-        Returns:
-            tuple: (input_dict, label_dict, weight_dict)
-        """
         # Get input data
         input_item = {key: value[index] for key, value in self.input.items()}
 
@@ -201,78 +190,6 @@ class LatentNODataset_time(io.Dataset):
     # Whether support batch indexing for speeding up fetching process.
     batch_index: bool = True
 
-    class normalizer:
-        def __init__(self, x, y1, y2):
-            self.x_flag = False
-            self.y1_flag = False
-            self.y2_flag = False
-            old_x_shape = x.shape
-            old_y1_shape = y1.shape
-            old_y2_shape = y2.shape
-            x = paddle.reshape(x, (-1, x.shape[-1]))
-            y1 = paddle.reshape(y1, (-1, y1.shape[-1]))
-            y2 = paddle.reshape(y2, (-1, y2.shape[-1]))
-            self.x_mean = paddle.mean(x, axis=0)
-            self.x_std = paddle.std(x, axis=0) + 1e-8
-            self.y1_mean = paddle.mean(y1, axis=0)
-            self.y1_std = paddle.std(y1, axis=0) + 1e-8
-            self.y2_mean = paddle.mean(y2, axis=0)
-            self.y2_std = paddle.std(y2, axis=0) + 1e-8
-            x = paddle.reshape(x, old_x_shape)
-            y1 = paddle.reshape(y1, old_y1_shape)
-            y2 = paddle.reshape(y2, old_y2_shape)
-
-        def is_apply_x(self):
-            return self.x_flag
-
-        def is_apply_y1(self):
-            return self.y1_flag
-
-        def is_apply_y2(self):
-            return self.y2_flag
-
-        def apply_x(self, x, device, inverse=False):
-            self.x_mean = self.x_mean.to(device)
-            self.x_std = self.x_std.to(device)
-
-            old_x_shape = x.shape
-            x = paddle.reshape(x, (-1, x.shape[-1]))
-            if not inverse:
-                x = (x - self.x_mean) / self.x_std
-                self.x_flag = True
-            else:
-                x = x * self.x_std + self.x_mean
-            x = paddle.reshape(x, old_x_shape)
-            return x
-
-        def apply_y1(self, y1, device, inverse=False):
-            self.y1_mean = self.y1_mean.to(device)
-            self.y1_std = self.y1_std.to(device)
-
-            old_y1_shape = y1.shape
-            y1 = paddle.reshape(y1, (-1, y1.shape[-1]))
-            if not inverse:
-                y1 = (y1 - self.y1_mean) / self.y1_std
-                self.y1_flag = True
-            else:
-                y1 = y1 * self.y1_std + self.y1_mean
-            y1 = paddle.reshape(y1, old_y1_shape)
-            return y1
-
-        def apply_y2(self, y2, device, inverse=False):
-            self.y2_mean = self.y2_mean.to(device)
-            self.y2_std = self.y2_std.to(device)
-
-            old_y2_shape = y2.shape
-            y2 = paddle.reshape(y2, (-1, y2.shape[-1]))
-            if not inverse:
-                y2 = (y2 - self.y2_mean) / self.y2_std
-                self.y2_flag = True
-            else:
-                y2 = y2 * self.y2_std + self.y2_mean
-            y2 = paddle.reshape(y2, old_y2_shape)
-            return y2
-
     def __init__(
         self,
         data_name: str,
@@ -295,31 +212,31 @@ class LatentNODataset_time(io.Dataset):
         self.transform_fn = transform_fn
 
         # Load data
-        data_file = f"./datas/{data_name}_{data_mode}.npy"
+        data_file = osp.join("datas", f"{data_name}_{data_mode}.npy")
         if not os.path.exists(data_file):
             raise FileNotFoundError(f"Data file not found: {data_file}")
 
         dataset = np.load(data_file, allow_pickle=True).tolist()
 
-        # Convert to tensors
-        x = paddle.to_tensor(dataset["x"], dtype="float32")
-        y1 = paddle.to_tensor(dataset["y1"], dtype="float32")
-        y2 = paddle.to_tensor(dataset["y2"], dtype="float32")
-        x = paddle.reshape(x, (x.shape[0], -1, x.shape[-1]))
-        y1 = paddle.reshape(y1, (y1.shape[0], -1, y1.shape[-1]))
-        y2 = paddle.reshape(y2, (y2.shape[0], -1, y2.shape[-1]))
+        x = dataset["x"].astype("float32")
+        y1 = dataset["y1"].astype("float32")
+        y2 = dataset["y2"].astype("float32")
+
+        x = x.reshape((x.shape[0], -1, x.shape[-1]))
+        y1 = y1.reshape((y1.shape[0], -1, y1.shape[-1]))
+        y2 = y2.reshape((y2.shape[0], -1, y2.shape[-1]))
         # Concatenate x and y1 if required
         if data_concat:
-            y1 = paddle.concat((x, y1), axis=-1)
+            y1 = np.concatenate((x, y1), axis=-1)
 
         # Initialize normalizer
-        self.normalizer = LatentNODataset.normalizer(x, y1, y2)
+        self.normalizer = Normalizer(x, y1, y2)
 
         # Apply normalization if required
         if data_normalize:
-            x = self.normalizer.apply_x(x, "cpu")
-            y1 = self.normalizer.apply_y1(y1, "cpu")
-            y2 = self.normalizer.apply_y2(y2, "cpu")
+            x = self.normalizer.apply_x(x)
+            y1 = self.normalizer.apply_y1(y1)
+            y2 = self.normalizer.apply_y2(y2)
 
         # Store data in input and label dictionaries following PaddleScience convention
         self.input = {
@@ -335,11 +252,6 @@ class LatentNODataset_time(io.Dataset):
         self._length = x.shape[0]
 
     def __getitem__(self, index: int):
-        """Get item following PaddleScience convention.
-
-        Returns:
-            tuple: (input_dict, label_dict, weight_dict)
-        """
         # Get input data
         input_item = {key: value[index] for key, value in self.input.items()}
 
