37a32d2e9ffb7ab57a933ea3ce58ccbc0c613451
Zhang Pu <92785660+zhangpu00000@users.noreply.github.com>
2025-10-24 13:28:17 +0800

    Update preformer.yaml

--- ppsci/data/dataset/era5sq_dataset.py
+++ ppsci/data/dataset/era5sq_dataset.py
@@ -25,7 +25,11 @@ from typing import Tuple
 import h5py
 import numpy as np
 import paddle
-import xarray as xr
+
+try:
+    import xarray as xr
+except ModuleNotFoundError:
+    pass
 from paddle import io
 from paddle import vision
 
